9f9b7f753eaa5e8589d661a1a60946065029fb80
Pokegear <christoph.ding@gmail.com>
2016-09-12 16:39:21 -0400

    Setting up stubs and tests for the kth-bst

---
+++ kth-bst/kth-bst.js
@@ -0,0 +1,14 @@
+// Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+var kthSmallest = function(root, k) {
+
+}
+
+// generate a BST
+function node(value, leftChild, rightChild) {
+
+}
+
+module.exports = {
+  kthSmallest: kthSmallest
+}
