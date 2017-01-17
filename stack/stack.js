457107a48befe10e21b6b0f1a4589f8aa7f58894
Pokegear <christoph.ding@gmail.com>
2017-01-16 21:18:53 -0500

    Begin implementing stacks.

--- stack/stack.js
+++ stack/stack.js
@@ -1,1 +1,22 @@
 // the implementation of a stack
+function Stack() {
+
+}
+
+Stack.prototype.push = function() {
+
+}
+
+Stack.prototype.pop = function() {
+
+}
+
+Stack.prototype.peek = function() {
+
+}
+
+Stack.prototype.isEmpty = function() {
+
+}
+
+var test = new Stack();
