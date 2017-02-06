ba7c2f5392586874131f7dd99c8caba095165b55
ironpup <christoph.ding@gmail.com>
2017-02-05 20:11:25 -0500

    Stub out functions to implmeent a queue using both linked list and with an array.

--- queue/queue.js
+++ queue/queue.js
@@ -1,1 +1,43 @@
-// the implementation of a queue
+// Queue Linked List
+function QueueLL () {
+
+}
+
+QueueLL.prototype.push = function() {
+
+}
+
+QueueLL.prototype.pop = function() {
+
+}
+
+Queue.prototype.peek = function() {
+
+}
+
+Queue.prototype.isEmpty = function() {
+
+}
+
+// Array implementation
+function QueueArray() {
+
+}
+
+QueueArray.prototype.push = function() {
+
+}
+
+QueueArray.prototype.pop = function() {
+
+}
+
+QueueArray.prototype.peek = function() {
+
+}
+
+QueueArray.prototype.isEmpty = function() {
+
+}
+
+// Basic Tests
