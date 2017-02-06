719bb45f783be6740356b74218d9b50db0f9a4ee
ironpup <christoph.ding@gmail.com>
2017-02-05 20:05:31 -0500

    Stubbed methods that must be exist for implementing two stacks using a single array.

---
+++ stack/twoStacksOneArray.js
@@ -0,0 +1,43 @@
+// Implement two stacks using a singleArray
+function twoStacksOneArray () {
+
+}
+
+// Shared Methods
+twoStacksOneArray.prototype.isFull = function() {
+  
+}
+
+// Stack One
+twoStacksOneArray.prototype.pushOne = function () {
+
+}
+
+twoStacksOneArray.prototype.popOne = function () {
+
+}
+
+twoStacksOneArray.prototype.peekOne = function () {
+
+}
+
+twoStacksOneArray.prototype.isEmptyOne = function () {
+
+}
+
+// Stack Two
+twoStacksOneArray.prototype.pushTwo = function () {
+
+}
+
+twoStacksOneArray.prototype.popTwo = function () {
+
+}
+
+twoStacksOneArray.prototype.peekTwo = function () {
+
+}
+
+twoStacksOneArray.prototype.isEmptyTwo = function () {
+
+}
