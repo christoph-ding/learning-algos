c2a25986140826f001417e2c80cc211bd77b8819
ironpup <christoph.ding@gmail.com>
2017-02-05 20:00:29 -0500

    Stub out functions for implementing a constant time access for the minimum and maximum elements in a stack.

---
+++ stack/minMaxStack.js
@@ -0,0 +1,59 @@
+// Stack Linked List
+function StackLL() {
+
+}
+
+StackLL.prototype.push = function() {
+
+}
+
+StackLL.prototype.pop = function() {
+
+}
+
+StackLL.prototype.peek = function() {
+
+}
+
+StackLL.prototype.isEmpty = function() {
+
+}
+
+// Min and Max must be in constant time, duh
+StackLL.prototype.getMin = function() {
+
+}
+
+StackLL.prototype.getMax = function() {
+
+}
+
+// Stack Array 
+function StackArray() {
+
+}
+
+StackArray.prototype.push = function() {
+
+}
+
+StackArray.prototype.pop = function() {
+
+}
+
+StackArray.prototype.peek = function() {
+
+}
+
+StackArray.prototype.isEmpty = function() {
+
+}
+
+// Min and Max must be in constant time, duh
+StackArray.prototype.getMin = function() {
+
+}
+
+StackArray.prototype.getMax = function() {
+  
+}
