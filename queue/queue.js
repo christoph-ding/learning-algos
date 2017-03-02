04ea366feb3668853ee194603ee019fdeb420884
ironpup <christoph.ding@gmail.com>
2017-03-01 21:13:21 -0500

    Implement a LL Queue.

--- queue/queue.js
+++ queue/queue.js
@@ -1,22 +1,46 @@
+// Node
+function stackNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 // Queue Linked List
 function QueueLL () {
-
+  this.frontOfQ = null;
+  this.backOfQ = null;
 }
 
-QueueLL.prototype.push = function() {
-
+QueueLL.prototype.enqueue = function(node) {
+  // there are elements
+  if (this.backOfQ) {
+    this.backOfQ.next = node;
+    this.backOfQ = node;
+  } else {
+  // there are no elements in the q
+    this.frontOfQ = node;
+    this.backOfQ = node;
+  }
 }
 
-QueueLL.prototype.pop = function() {
-
+QueueLL.prototype.dequeue = function() {
+  // there are elements
+  if (this.frontOfQ) {
+    var deQueuedNode = this.frontOfQ;
+    this.frontOfQ = this.frontOfQ.next;
+    return deQueuedNode;
+  }
+  // there are no elements in the q
+  {
+    return null;
+  }
 }
 
-Queue.prototype.peek = function() {
-
+QueueLL.prototype.peek = function() {
+  return this.frontOfQ;
 }
 
-Queue.prototype.isEmpty = function() {
-
+QueueLL.prototype.isEmpty = function() {
+  return this.frontOfQ === null;
 }
 
 // Array implementation
@@ -39,5 +63,16 @@ QueueArray.prototype.peek = function() {
 QueueArray.prototype.isEmpty = function() {
 
 }
-
 // Basic Tests
+var nodeOne = new stackNode(3);
+var nodeTwo = new stackNode(4);
+var nodeFour = new stackNode(6);
+
+var queueOneLL = new QueueLL();
+queueOneLL.enqueue(nodeOne);
+queueOneLL.enqueue(nodeTwo);
+queueOneLL.enqueue(nodeFour);
+console.log(queueOneLL.dequeue());
+console.log(queueOneLL.dequeue());
+
+console.log(queueOneLL);
