6735e7ab6d6705928b8654c1334f7d88fafab085
ironpup <christoph.ding@gmail.com>
2017-03-05 15:58:33 -0500

    Write some tests/

--- stack/stackWQueues.js
+++ stack/stackWQueues.js
@@ -8,27 +8,35 @@ function stackWQueuesSlowPop() {
   this.secondQ = this.Q2;
 }
 
-stackWQueuesSlowPop.prototype.transferBetweenStacks() {
+stackWQueuesSlowPop.prototype.transferBetweenStacks = function() {
   while (this.mainQ.length > 1) {
-    this.secondQ.push(this.mainQ.unshift());
+    this.secondQ.push(this.mainQ.shift());
   }
   var temp = this.mainQ;
   this.mainQ = this.secondQ;
   this.secondQ = temp;  
 }
 
-stackWQueuesSlowPop.prototype.push(val) {
+stackWQueuesSlowPop.prototype.push = function(val) {
   this.mainQ.push(val);
 }
 
-stackWQueuesSlowPop.prototype.pop() {
+stackWQueuesSlowPop.prototype.pop = function() {
   this.transferBetweenStacks();
   return this.secondQ.pop();
 }
 
-stackWQueuesSlowPop.prototype.isEmpty() {
+stackWQueuesSlowPop.prototype.isEmpty = function() {
   return this.Q1.length + this.Q2.length === 0;
 }
 
 // tests
 var testStackOne = new stackWQueuesSlowPop();
+testStackOne.push(1);
+testStackOne.push(2);
+testStackOne.push(3);
+console.log(testStackOne.pop());
+console.log(testStackOne.pop());
+testStackOne.push(4);
+console.log(testStackOne.pop());
+console.log(testStackOne.pop());
