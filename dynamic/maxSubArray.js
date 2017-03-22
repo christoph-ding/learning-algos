acda68235b5bbaca1a1fc8755406a07753690c62
ironpup <christoph.ding@gmail.com>
2017-03-21 21:30:39 -0400

    Start the max sub array with dynamic programming problem.

---
+++ dynamic/maxSubArray.js
@@ -0,0 +1,16 @@
+function maxSubArray(numberList, k) {
+  // what if k is larger than number of items in list?
+
+  // we use a helper queue
+
+  // iterate through the numberList
+    // for each, apply the comparison
+
+
+  // go through the helper queue and find the max
+
+}
+
+// tests
+var listOne = [1,2,3,4,5,6,7,8,9];
+var listTwo = [-10, 23, 100, -30, 24, 93, 1, -9, 23, -4, 11];
