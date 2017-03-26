ba7204014d7b94771547cd98b8d748f4a65c62ef
ironpup <christoph.ding@gmail.com>
2017-03-26 15:12:16 -0400

    Implemented maximum subarray using Kidane's algorithm.  In the process of testing.

--- dynamic/maxSubArray.js
+++ dynamic/maxSubArray.js
@@ -1,10 +1,23 @@
-function maxSubArray(numberList, k) {
-  // what if k is larger than number of items in list?
-
+function maxSubArray(numberList) {
   // we use a helper queue
+  var helper = [];
 
   // iterate through the numberList
-    // for each, apply the comparison
+  for (var i = 0; i < numberList.length; i++) {
+    var currentNumber = numberList[i];
+
+    // apply Kidane's algorithm
+    if (i === 0) {
+      var numberToAddToHelper = currentNumber;
+    } else {
+      var previousMaxSum = helper[i - 1];
+      // add to the helped queue based upon a comparison
+      // either add the currentNumber number to the current run sum, or start a new run
+      var numberToAddToHelper = Math.max(previousMaxSum + currentNumber, currentNumber);
+    }
+    helper.push(numberToAddToHelper);
+  }
+
 
 
   // go through the helper queue and find the max
@@ -12,5 +25,7 @@ function maxSubArray(numberList, k) {
 }
 
 // tests
-var listOne = [1,2,3,4,5,6,7,8,9];
+var listOne = [-1, 5, 4, -2, 10, -1];
 var listTwo = [-10, 23, 100, -30, 24, 93, 1, -9, 23, -4, 11];
+
+var maxOne = maxSubArray(listOne);
