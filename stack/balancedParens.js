eca7ad14dc853fa20b5582b6c4e02a465265af26
ironpup <christoph.ding@gmail.com>
2017-03-27 21:04:16 -0400

    Start balanced Parens problem.

---
+++ stack/balancedParens.js
@@ -0,0 +1,19 @@
+// 
+function balancedParens(inputStr) {
+  // a hash for the 'opening character' and their corresponding 'closing character'
+
+  // helper data structure
+
+  // iterate through inputStr
+    // do something when we encounter 'open'
+
+    // do something when we encounter 'closed'
+  
+  // at the end ... is helpder empty?
+
+}
+
+// tests
+var stringOne = 'abc'; // true
+var stringTwo = '(asdasd)'; // true
+var stringThree = '[asdfasdfadsfadf' // false
