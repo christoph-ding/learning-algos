4d418e192606a64fabf0627dc9e5d46a1820cf66
ironpup <christoph.ding@gmail.com>
2017-03-28 21:16:31 -0400

    Start merge-sort and wrote preliminary tests.

---
+++ merge-sort/mergesort.js
@@ -0,0 +1,17 @@
+// the function
+function mergeSort(list) {
+
+};
+
+function breakApart(list) {
+
+};
+
+function mergeTwo(listOne, listTwo) {
+
+};
+
+// the tests
+var listOne = [1]; // 1
+var listTwo = [3, 2, 1]; // [3, 2, 1]
+var listThree = [10, 11, 3, 4, 9, 2, 5]; // [2, 3, 4, 5, 9, 10, 11]
