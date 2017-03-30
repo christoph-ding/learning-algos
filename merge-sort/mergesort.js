a4756b44dc8fd26f1e8be13ddf206b87f8838b4d
ironpup <christoph.ding@gmail.com>
2017-03-29 20:18:31 -0400

    In progress on the merge-sort.

--- merge-sort/mergesort.js
+++ merge-sort/mergesort.js
@@ -1,13 +1,31 @@
 // the function
 function mergeSort(list) {
 
+  // if the list is more than a single element, break it apart
+    if (list.length > 1) {
+      // left and right half of the list
+      var halfIndex = Math.floor(list.length / 2);
+      var left = list.slice(0, halfIndex);
+      var right = list.slice(halfIndex);
+
+      // recursion!
+      var sortedLeft = mergeSort(left);
+      var sortedRight = mergeSort(right);
+
+      // merge the two
+      var mergedSortedList = combine(sortedLeft, sortedRight);
+    } else if (list.length === 1) {
+      return list;
+    }
 };
 
-function breakApart(list) {
+function combine(listOne, listTwo) {
+  
+  // output is the sorted combination of both input lists
+  var output = [];
 
-};
+  // 
 
-function mergeTwo(listOne, listTwo) {
 
 };
 
