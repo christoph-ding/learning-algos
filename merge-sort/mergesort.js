827a485b7d76b9ed37c60d73f411fd9e459ef985
ironpup <christoph.ding@gmail.com>
2017-03-30 21:15:47 -0400

    Implement the combine of merge sort and being next greater element.

--- merge-sort/mergesort.js
+++ merge-sort/mergesort.js
@@ -20,16 +20,43 @@ function mergeSort(list) {
 };
 
 function combine(listOne, listTwo) {
-  
   // output is the sorted combination of both input lists
-  var output = [];
+  var combinedSorted = [];
 
-  // 
+  while (listOne.length && listTwo.length) {
+    // compare the leftmost - and therefore, smallest of each sorted list
+    var listOneFirst = listOne[0];
+    var listTwoFirst = listTwo[0];
 
+    if (listOneFirst < listTwoFirst) {
+      combinedSorted.push(listOne.shift());
+    } else if (listTwoFirst < listOneFirst) {
+      combinedSorted.push(listTwo.shift());
+    }
+  }
 
+  // put the 'rest' of one of the lists into combinedSorted
+  if (listOne.length === 0) {
+    while (listTwo.length) {
+      combinedSorted.push(listTwo.shift());
+    }
+  } else if (listTwo.length === 0 ) {
+    while (listOne.length) {
+      combinedSorted.push(listOne.shift());
+    }
+  }
+
+  return combinedSorted
 };
 
 // the tests
 var listOne = [1]; // 1
 var listTwo = [3, 2, 1]; // [3, 2, 1]
 var listThree = [10, 11, 3, 4, 9, 2, 5]; // [2, 3, 4, 5, 9, 10, 11]
+
+// test functions seperately
+var sortOne = [1, 3, 5];
+var sortTwo = [ 2, 4, 9];
+var sortThree = [6, 7, 11, 12];
+
+console.log(combine(sortOne, sortTwo));
---
+++ stack/NextGreaterElement.js
@@ -0,0 +1,7 @@
+function nextGreaterElement(inputList) {
+
+}
+
+// tests
+var listOne = [4, 5, 2, 25] // [5, 25, 25, -1]
+var listTwo = [1, 2, 3, 4, 25] // [2, 3, 4, 25, -1]
