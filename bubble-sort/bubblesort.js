c7c5349207746e8e090d7bb340377f4b18477ab1
ironpup <christoph.ding@gmail.com>
2017-04-02 16:51:41 -0400

    Start stockpriceeasy and bubble sort.

---
+++ bubble-sort/bubblesort.js
@@ -0,0 +1,13 @@
+function bubbleSort(input) {
+  // no output because we bubbleSort in place
+
+  // iterate through input,
+    // compare and if applicable,
+
+    // swap
+
+}
+
+// tests
+var testOne = [1, 21, 30, 0];
+var testTwo = [10, 8, 2, 6, 3];
---
+++ dynamic/stockPriceEasy.js
@@ -0,0 +1,12 @@
+function stockEasy(prices) {
+  
+
+
+
+
+
+}
+
+// tests
+var listOne = [9, 10, 21, 4, 7, 23];
+var listTwo = [22, 24, 14, 6, 28, 10];
