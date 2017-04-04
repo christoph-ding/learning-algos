a208455fdb13e17a7b7e53763b110ed462a4b6b8
ironpup <christoph.ding@gmail.com>
2017-04-03 22:07:20 -0400

    Implemented merge-sort.

--- merge-sort/mergesort.js
+++ merge-sort/mergesort.js
@@ -13,7 +13,7 @@ function mergeSort(list) {
       var sortedRight = mergeSort(right);
 
       // merge the two
-      var mergedSortedList = combine(sortedLeft, sortedRight);
+      return mergedSortedList = combine(sortedLeft, sortedRight);
     } else if (list.length === 1) {
       return list;
     }
@@ -52,11 +52,11 @@ function combine(listOne, listTwo) {
 // the tests
 var listOne = [1]; // 1
 var listTwo = [3, 2, 1]; // [3, 2, 1]
-var listThree = [10, 11, 3, 4, 9, 2, 5]; // [2, 3, 4, 5, 9, 10, 11]
+var listThree = [10, 1, 17, 11, 3, 4, 24,33, 9, 2, 5]; // [2, 3, 4, 5, 9, 10, 11]
 
 // test functions seperately
 var sortOne = [1, 3, 5];
 var sortTwo = [ 2, 4, 9];
 var sortThree = [6, 7, 11, 12];
 
-console.log(combine(sortOne, sortTwo));
+console.log(mergeSort(listThree))
