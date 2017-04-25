61cebb7b98798d807b41cd8f91e2810c06d4ae28
ironpup <christoph.ding@gmail.com>
2017-04-24 21:18:43 -0400

    Stub out the recursive algo for left-bottom-most node of BT.

---
+++ binaryTree/bottomLeftMost.js
@@ -0,0 +1,17 @@
+function bottomLeftMost(node, depth) {
+  // find left child's bottom left most node
+
+  // find right child's bottom right most node
+
+  // compare
+
+  // or ... return oneself
+
+}
+
+// nodes
+function node(val) {
+  this.val = val;
+}
+
+// tests
