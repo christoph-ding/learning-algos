13f5f55c4c9140a17701b7f5aa886c9c270f2227
ironpup <christoph.ding@gmail.com>
2017-04-25 21:07:15 -0400

    Implemented algo for bottomLeftMost node of tree, need to test.

--- binaryTree/bottomLeftMost.js
+++ binaryTree/bottomLeftMost.js
@@ -1,11 +1,28 @@
 function bottomLeftMost(node, depth) {
-  // find left child's bottom left most node
+  depth = depth + 1 || 0
 
+  // if there are no children, return oneself
+  if (!(node.lc || node.rc)) {
+    return {node: node, depth: depth}
+  }
+
+  // find left child's bottom left most node
+  if (node.lc) {
+    var bottomLeftMostLC = bottomLeftMost(node.lc, depth)
+  }
   // find right child's bottom right most node
+  if (node.rc) {
+    var bottomLeftMostRC = bottomRightMost(node.rc, depth)
+  }
 
   // compare
+  if (!(node.rc) || bottomLeftMostLC.depth > bottomLeftMostRC.depth) {
+    return bottomLeftMostLC
+  } else if (!(node.lc) || bottomLeftMostRC.depth > bott) {
+    return bottomLeftMostRC
+  }
 
-  // or ... return oneself
+  // or ... return oneself 
 
 }
 
