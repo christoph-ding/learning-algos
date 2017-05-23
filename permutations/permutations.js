6770be9f48e8b8057e1bf83ed1914b1037f70a5d
ironpup <christoph.ding@gmail.com>
2017-05-22 20:55:45 -0400

    Start on permutation problem.

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -1,6 +1,17 @@
 function permutations (input) {
+  var output = []
 
+  for (var i = 0; i < input.length; i++) {
+    var currentElem = input[i]
+    var restOfElem = input.splice()
+
+  }
+  return output
 }
 
 // test cases
-var listOne = [1,2,3];
+var listOne = [1,2,3]
+// permutationsOne = permutations(listOne)
+// console.log(permutations)
+
+console.log(listOne.slice(0,1).concat(listOne.slice(1)))
