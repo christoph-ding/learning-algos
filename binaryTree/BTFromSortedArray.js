17d65a3f8288236c4262365e0dedf5d9c36391f1
ironpup <christoph.ding@gmail.com>
2017-06-01 20:56:07 -0400

    Start BST

--- binaryTree/BTFromSortedArray.js
+++ binaryTree/BTFromSortedArray.js
@@ -1,3 +1,15 @@
+function treeNode(val, lc, rc, parent) {
+  this.val = val || null
+  this.lc = lc || null
+  this.rc = rc || null
+  this.parent = parent || null
+}
+
+
+function BST() {
+
+}
+
 function BTFromSortedArray(input) {
 
 }
