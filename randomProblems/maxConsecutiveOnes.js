2054662ad996e26b44911982569b67d24d4b6cfc
ironpup <christoph.ding@gmail.com>
2017-06-11 13:27:37 -0400

    Implement max consecutive ones.

--- randomProblems/maxConsecutiveOnes.js
+++ randomProblems/maxConsecutiveOnes.js
@@ -1,16 +1,27 @@
 function maxConsecutiveOnes(input) {
-  var maxSoFar = 0;
-  var run = 0;
+  var longestSoFar = 0;
+  var currentRun = 0;
 
   input.forEach( function(elem) {
-    if ()
-
+    if (elem === 1) {
+      currentRun++
+      compareAndUpdate()
+    } else {
+      compareAndUpdate()
+      currentRun = 0
+    }
   })
 
-  return run;
+  function compareAndUpdate () {
+    if (currentRun > longestSoFar) {
+      longestSoFar = currentRun;
+    }
+  }
+
+  return longestSoFar;
 }
 
 // tests
-var listOne = [1,1,0,0,1,1,1];
+var listOne = [1,1,0,0,1,1,1,1,0,1,1,1,1,1];
 
 console.log(maxConsecutiveOnes(listOne));
