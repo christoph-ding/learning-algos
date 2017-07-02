4fcc3b4ceb44422cb6111099e2844eea3c209e2d
ironpup <christoph.ding@gmail.com>
2017-07-02 16:06:09 -0400

    Add methods to bst, start implementing insert method recursively.

--- bst/bst-2.js
+++ bst/bst-2.js
@@ -4,19 +4,70 @@ var BinarySearchTree = function(root) {
 }
 
 var Node = function(value, lc, rc) {
-
   // number
   this.value = value
-
   // Nodes
   this.lc = lc
   this.rc = rc
 }
 
-// testing
-var nodeTwo = new Node(5)
-var nodeOne = new Node(8, nodeTwo)
+BinarySearchTree.prototype.insert = function(newNode) {
 
+  var currentNode = this.root
+  
+  function _insertion() {
+    // insert based upon comparsion
+
+    // no children
+    if (!currentNode.lc && !currentNode.rc) {
+      if (newNode.value < currentNode.value) {
+        currentNode.lc = newNode
+      } else if (newNode.value > currentNode.value) {
+        currentNode.rc = newNode
+      }
+    }
+    // has children
+      // compare ...
+        // insert left
+        // insert right
+  }
+
+  function _compareAndInsert(value) {
+
+  }
+
+  _insertion()
+
+}
+ 
+BinarySearchTree.prototype.contains = function() {
+
+}
+
+BinarySearchTree.prototype.depthFirstTraversal = function() {
+
+}
+
+// testing
+
+// var nodeTwo = new Node(5)
+// var nodeOne = new Node(8, nodeTwo)
+
+// var BSTOne = new BinarySearchTree(nodeOne)
+
+// test the constructor for node and BST
+// console.log(BSTOne)
+
+// test BST class has relevant BST methods
+// var BSTProperties = Object.getOwnPropertyNames(BinarySearchTree.prototype)
+// console.log(BSTProperties.indexOf('insert') !== -1)
+// console.log(BSTProperties.indexOf('contains') !== -1)
+// console.log(BSTProperties.indexOf('depthFirstTraversal') !== -1)
+
+// test insert is correct
+var nodeOne = new Node(8)
 var BSTOne = new BinarySearchTree(nodeOne)
 
+var newNode = new Node(11)
+BSTOne.insert(newNode)
 console.log(BSTOne)
