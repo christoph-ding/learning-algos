85e6242fe3efd56bea058a710e2328b67058ee4e
ironpup <christoph.ding@gmail.com>
2017-07-16 10:43:30 -0400

    Begin bulbSwitcher and write some tests and conjecture.

---
+++ randomProblems/bulbSwitcher.js
@@ -0,0 +1,16 @@
+function bulbSwitcher(n) {
+  // all off initially
+
+  // for the nth round, toggle every nth bulb
+
+  // how many are on at the end?
+
+}
+
+// tests
+console.log(bulbSwitcher(1)) // 1
+console.log(bulbSwitcher(2)) // 1
+console.log(bulbSwitcher(3)) // 1
+console.log(bulbSwitcher(4)) // 2
+
+// thinking / conjecture
