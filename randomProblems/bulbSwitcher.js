8a4b25898e8e6b5f85b6d89caca99d5852762ea1
ironpup <christoph.ding@gmail.com>
2017-07-16 10:57:34 -0400

    Complete bulbSwitcher.

--- randomProblems/bulbSwitcher.js
+++ randomProblems/bulbSwitcher.js
@@ -1,16 +1,37 @@
 function bulbSwitcher(n) {
-  // all off initially
+  // how many perfect squares are between 1 and n, inclusive?
+  var numberOfRoots = 0
+  var counter = 1
 
-  // for the nth round, toggle every nth bulb
+  while (counter <= n) {
+    _isSquare(counter)
+    counter++
+  }
 
-  // how many are on at the end?
+  return numberOfRoots
 
+  function _isSquare(num) {
+    var root = Math.sqrt(num)
+    if (root % 1 === 0) {
+      numberOfRoots++
+    }
+  }
 }
 
-// tests
+// the entire algorithm
 console.log(bulbSwitcher(1)) // 1
 console.log(bulbSwitcher(2)) // 1
 console.log(bulbSwitcher(3)) // 1
 console.log(bulbSwitcher(4)) // 2
 
 // thinking / conjecture
+
+// all bulbs are turned off in 'pairs'
+// whenever a bulb I is toggled by the nth run,
+// it is toggled again (two toggles 'resets' a bulb)
+// by the complement of n.
+// the EXCEPTION to this are perfect squares.
+// console.log(bulbSwitcher._isSquare(1))
+
+
+
