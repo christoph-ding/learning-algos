73a9d429a1d0a60cac19a94fc19d13d6d020d736
ironpup <christoph.ding@gmail.com>
2017-07-24 20:54:46 -0400

    Continuted RelativeRisk.

--- randomProblems/relativeRanks.js
+++ randomProblems/relativeRanks.js
@@ -1,12 +1,18 @@
 function relativeRanks(input) {
+  var helper = {}  
   var sortedList = input.sort().reverse()
-  console.log(sortedList)
 
+  input.forEach(function(elem, i) {
+    helper[elem] = i
+    console.log(elem, i)
+  })
 
 }
 
 // tests
-var listOne = [1, 2, 3]
+var listOne = [1, 3, 2]
+
+          //  [3, 2, 1]
 var listTwo = [3, 2, 1]
 
 console.log(relativeRanks(listOne)) // [3, 2, 1]
