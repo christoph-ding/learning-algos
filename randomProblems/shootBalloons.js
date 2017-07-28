0129584ff06b1e06572041d4635bb7ddadf48623
ironpup <christoph.ding@gmail.com>
2017-07-28 17:31:56 -0400

    Start shoot balloons.

--- randomProblems/shootBalloons.js
+++ randomProblems/shootBalloons.js
@@ -1,4 +1,11 @@
 function shootBalloons(balloons) {
+
+  // bins ... fill up
+  // _ 1-2 1-2 _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+  // check: is there a balloon there?  
+  // does that right's bigger > this left?
+
   balloons.forEach(function(balloon) {
     var size = balloon[1] - balloon[0]
     console.log('start is: ', balloon[0], ' size: ', size)
