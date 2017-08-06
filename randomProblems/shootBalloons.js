487f0a6a0b5ee649a41639d7fcbd46ae4f262b5b
ironpup <christoph.ding@gmail.com>
2017-08-06 16:31:03 -0400

    Pseudocode for shooting balloons problem.

--- randomProblems/shootBalloons.js
+++ randomProblems/shootBalloons.js
@@ -1,15 +1,15 @@
 function shootBalloons(balloons) {
+  // helper data structure
 
-  // bins ... fill up
-  // _ 1-2 1-2 _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
-  // check: is there a balloon there?  
-  // does that right's bigger > this left?
-
-  balloons.forEach(function(balloon) {
-    var size = balloon[1] - balloon[0]
-    console.log('start is: ', balloon[0], ' size: ', size)
-  })
+  // iterate through balloons - balloon
+    // at balloon, we ask:
+      // does this overlap with an existing balloon / get popped by an existing arrow
+      // if so:
+        // don't add arrow count
+      // if not:
+        // add new arrow
+      // remember this balloon
 }
 
 var testOne = [ [1,2], [2, 6], [3, 4], [2, 7] ] 
