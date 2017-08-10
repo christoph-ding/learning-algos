23bfba7a8ffc0baa12c266255e3427b06d83a3ad
ironpup <christoph.ding@gmail.com>
2017-08-09 21:09:54 -0400

    Start combinationSumIV

---
+++ randomProblems/combinationSumIV.js
@@ -0,0 +1,12 @@
+function comboSum(nums, target) {
+  var answer = []
+
+
+
+
+  // call the inner function
+  return answers
+}
+
+var testCaseOne = {nums: [1, 2, 3], target: 4}
+var answerOne = comboSum(testCaseOne.nums, testCaseOne.target)
