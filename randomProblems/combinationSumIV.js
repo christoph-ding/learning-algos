e4cd071b9abf116a02f14cc87550080748da9de0
ironpup <christoph.ding@gmail.com>
2017-08-13 11:04:37 -0400

    HAve a rough answer for combinationSumIV that works mostly.  Reviewing and adding more tests.

--- randomProblems/combinationSumIV.js
+++ randomProblems/combinationSumIV.js
@@ -1,15 +1,30 @@
 function comboSum(nums, target) {
   var answers = []
 
-  function gatherCombos(newTarget) {
+  // all function calls of gatherCombos will manipulate this DS
+  // each function call will add, then remove from singleCombo
+  var singleCombo = []
 
-
-
-
+  function gatherCombos(innerTarget) {
+    console.log('combo: ', singleCombo, ' innerTarget: ', innerTarget)
+    if (innerTarget === 0) {
+      console.log('done!')
+      answers.push(singleCombo)
+    } else {      
+      nums.forEach(function(num) {
+        if (num <= innerTarget) {
+          console.log('making recursive call!')
+          singleCombo.push(num)
+          gatherCombos(innerTarget - num)
+          singleCombo.pop()
+        }          
+      })
+    }
+  }
 
   gatherCombos(target)
   return answers
 }
 
-var testCaseOne = {nums: [1, 2, 3], target: 4}
+var testCaseOne = {nums: [1, 2], target: 2}
 var answerOne = comboSum(testCaseOne.nums, testCaseOne.target)
