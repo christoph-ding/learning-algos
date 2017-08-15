bdb66261b143d7ad682713cd4ad328a022b723ae
ironpup <christoph.ding@gmail.com>
2017-08-14 21:21:27 -0400

    Continue on one solution and testing for binaryCalculator.

--- randomProblems/binaryCalculator.js
+++ randomProblems/binaryCalculator.js
@@ -2,15 +2,21 @@ function binaryCalculator(a, b) {
   var aVal = 0
   var aAsDigits = a.split('')
 
+  var bVal = 0
+  var bAsDigits = b.split('')
 
+  aAsDigits.forEach(function(digit, indx) {
+    var digitVal = convertToDecimal(digit, indx + 1)
+    aVal += digitVal
+  })
+
+  console.log(aVal)
 }
 
-function convertToDecimal(digit, indx) {
-  var digitVal = Number(digit) * Math.pow(2, indx + 1)
+function convertToDecimal(digit, twosPlace) {
+  var digitVal = Number(digit) * Math.pow(2, twosPlace)
   return digitVal
 }
 
-// var testOne = [1, 11]
-// binaryCalculator(testOne[0], testOne[1])
-
-console.log(convertToDecimal('1', 2))
+var testOne = ['11', '11']
+binaryCalculator(testOne[0], testOne[1])
