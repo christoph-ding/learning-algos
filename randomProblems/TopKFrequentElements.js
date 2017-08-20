b04544168ff23982c5bdb69fbdf6ad2cd081d705
ironpup <christoph.ding@gmail.com>
2017-08-20 12:06:03 -0400

    Finished step 2 and started step 3.  Add more tests.

--- randomProblems/TopKFrequentElements.js
+++ randomProblems/TopKFrequentElements.js
@@ -2,18 +2,42 @@ function topKFrequentElements(elemList, k) {
   var output = []
 
   // step 1, O(n): get a DS with the frequency of each unique elem
+  var frequencyHash = {}
 
+  elemList.forEach(placeInFrequencyHash)
 
   // step 2, O(n): place each elem:frequency pair into a helper list that naturally sorts
+  var helperSortList = []
 
+  var uniqueElems = Object.keys(frequencyHash)
+  uniqueElems.forEach(placeInFrequencySortList)
 
   // step 3, O(n): use 'sorted', helper list to build output
 
+  helperSortList.reverse()
+  helperSortList.forEach(function(elem) {
+    console.log(elem)
+  })
 
   return output
+
+  // helper functions
+  function placeInFrequencyHash(elem) {
+    if (frequencyHash.hasOwnProperty(elem)) {
+      frequencyHash[elem]++
+    } else {
+      frequencyHash[elem] = 1
+    }
+  }
+
+  function placeInFrequencySortList(uniqueElem) {
+    var elemFrequency = frequencyHash[uniqueElem]
+    helperSortList[elemFrequency] = uniqueElem
+  }
+
 }
 
 // tests
-var testOne = [1,1,2]
-console.log(topKFrequentElements(testOne, 1) // [1]
-console.log(topKFrequentElements(testOne, 2) // [1, 2]
+var testOne = [1, 1, 1, 2]
+console.log(topKFrequentElements(testOne, 1)) // [1]
+// console.log(topKFrequentElements(testOne, 2) // [1, 2]
