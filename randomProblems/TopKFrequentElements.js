2986cec941d6edfdd457281883c9257f2da4817a
ironpup <christoph.ding@gmail.com>
2017-08-20 11:49:46 -0400

    Start topKFrequentElements problem.  Stubbed out problem and write some tests.

---
+++ randomProblems/TopKFrequentElements.js
@@ -0,0 +1,12 @@
+function topKFrequentElements(elemList, k) {
+  var output = []
+
+
+
+  return output
+}
+
+// tests
+var testOne = [1,1,2]
+console.log(topKFrequentElements(testOne, 1) // [1]
+console.log(topKFrequentElements(testOne, 2) // [1, 2]
