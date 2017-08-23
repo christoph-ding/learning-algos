b5e36bb4d7e71c2d07327e64b6ac2e025acc33d6
ironpup <christoph.ding@gmail.com>
2017-08-22 20:45:54 -0400

    Begin work on linkedListNodes and a function that makes a linkedList from a list of values.

--- linked-list/palindrome-check.js
+++ linked-list/palindrome-check.js
@@ -1,13 +1,34 @@
 function linkedList(head, tail) {
-
+  this.head = head
+  this.tail = tail
 }
 
 function linkedListNode(val, next) {
+  this.val = val
+  this.next = next || null 
+}
+
+function generateLLNodes(input) {
+  input.forEach(function(val, indx) {
+    var newNode = new linkedListNode(val)
+
+
+    if (indx < )
+    console.log(val, '    ', indx)
+  })
+}
+
+function checkPalindrome(linkedList) {
 
 }
 
-function checkPalindrome(linked-list) {
 
-}
 
 // tests
+var nodeOne = new linkedListNode(1, null)
+var nodeTwo = new linkedListNode(2, nodeOne)
+var nodeThree = new linkedListNode(2, nodeTwo)
+
+var nodeList = [1,2,3,4,5,6,7,8,9]
+generateLLNodes(nodeList)
+
