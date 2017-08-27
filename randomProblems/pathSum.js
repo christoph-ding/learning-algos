fe7b8a53792206455515c2e84bc5358a17f904fc
ironpup <christoph.ding@gmail.com>
2017-08-27 17:32:28 -0400

    Started pathSum of a BST.  Starting by quickly implementing functions neccesary to quickly generate valid BSTs.

---
+++ randomProblems/pathSum.js
@@ -0,0 +1,18 @@
+function pathSum(root, target) {
+
+}
+
+// we need to define a BST first ... blagh!
+function treeNode(val, left, right) {
+
+}
+
+function BSTInsert(root, val) {
+
+}
+
+function generateBST(root, listOfValues) {
+
+}
+
+// tests
