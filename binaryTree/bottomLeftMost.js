5074b9c5d11c4edaff801ab1101420922df80ba6
ironpup <christoph.ding@gmail.com>
2017-09-03 11:24:08 -0400

    Finished testing bottomLeftMost of BT.

--- binaryTree/bottomLeftMost.js
+++ binaryTree/bottomLeftMost.js
@@ -8,22 +8,22 @@ function bottomLeftMost(node, depth) {
 
   // find left child's bottom left most node
   if (node.lc) {
+    console.log('looking in LC of ', node.val)
     var bottomLeftMostLC = bottomLeftMost(node.lc, depth)
   }
   // find right child's bottom right most node
   if (node.rc) {
-    var bottomLeftMostRC = bottomRightMost(node.rc, depth)
+    console.log('looking in RC of ', node.val)
+    var bottomLeftMostRC = bottomLeftMost(node.rc, depth)
   }
 
   // compare
+  console.log('comparing ... lc: ', bottomLeftMostLC, ' rc: ', bottomLeftMostRC)
   if (!(node.rc) || bottomLeftMostLC.depth > bottomLeftMostRC.depth) {
     return bottomLeftMostLC
   } else if (!(node.lc) || bottomLeftMostRC.depth > bott) {
     return bottomLeftMostRC
   }
-
-  // or ... return oneself 
-
 }
 
 // nodes
@@ -55,4 +55,9 @@ treeOneRoot.lc = setOneNodes[1]
 setOneNodes[3].rc = setOneNodes[4]
 treeOneRoot.rc = setOneNodes[3]
 
-console.log(treeOneRoot)
+// console.log(treeOneRoot)
+
+// var leftBottomOne = bottomLeftMost(treeOneRoot)
+var testNodeTwo = new Node(2)
+var testNodeOne = new Node(1, testNodeTwo)
+console.log(bottomLeftMost(testNodeOne))
