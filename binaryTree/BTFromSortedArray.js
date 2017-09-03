f3b3a266182d6a2443876ff975d5b858a09af48f
ironpup <christoph.ding@gmail.com>
2017-09-03 15:58:14 -0400

    Found algorithm for BSTFromSortedArray.  Wrote psuedocode and some tests.

--- binaryTree/BTFromSortedArray.js
+++ binaryTree/BTFromSortedArray.js
@@ -1,18 +1,38 @@
-function treeNode(val, lc, rc, parent) {
+function treeNode(val, lc, rc) {
   this.val = val || null
   this.lc = lc || null
   this.rc = rc || null
-  this.parent = parent || null
 }
 
+function BSTFromSortedArray(inputList, leftBound, rightBound) {
+  // find bounds of the inputList - 'left tree', 'the root of the current tree', 'right tree'
+  var middleIndex = Math.floor(inputList.length / 2)
+  var leftBound;
+  var rightBound;
 
-function BST() {
-  
+  // currentRoot = new treeNode(inputLit[middleIndex])
+
+  // if there is a leftTree:
+    // rootOfLeftTree = BSTFromSortedArray(inputList, newLeftBound, newRightBound)
+    // currentRoot.lc = rootOfLeftTree
+  // if there is a rightTree:
+    // rootOfRightTree = BSTFromSortedArray(inputList, newLeftBound, newRightBound)  
+    // currentRoot.rc = rootOfRightTree
+
+  // return currentRoot
+
 }
 
-function BTFromSortedArray(input) {
-
+// is this neccesary?
+function createNodesFromValues(listOfValues) {
+  var nodesList = listOfValues.map(function(value) {
+    return new treeNode(value)
+  })
+  return nodesList
 }
 
 // test
+
 var test = [1, 2, 3, 4, 5, 6]
+
+BSTFromSortedArray(test)
