33cd3cced4506542400edaba74605b0b0e44712c
ironpup <christoph.ding@gmail.com>
2017-09-04 14:17:39 -0400

    Finished BST From Sorted Array.

--- binaryTree/BTFromSortedArray.js
+++ binaryTree/BTFromSortedArray.js
@@ -6,11 +6,11 @@ function treeNode(val, lc, rc) {
 
 function BSTFromSortedArray(inputList, leftBound, rightBound) {
   // find bounds of the inputList - 'left tree', 'the root of the current tree', 'right tree'
-  var middleIndex = Math.floor(inputList.length / 2)
   var leftBound = leftBound || 0
   var rightBound = rightBound || inputList.length - 1
+  var middleIndex = Math.floor((rightBound + leftBound) / 2)
 
-  currentRoot = new treeNode(inputList[middleIndex])
+  var currentRoot = new treeNode(inputList[middleIndex])
 
   console.log('middleIndex: ', middleIndex)
   console.log('leftBound: ', leftBound)
@@ -19,14 +19,18 @@ function BSTFromSortedArray(inputList, leftBound, rightBound) {
   // if there is a leftTree:
   if (leftBound < middleIndex) {
     console.log('a left tree exists!')
-    rootOfLeftTree = BSTFromSortedArray(inputList, newLeftBound, newRightBound)
+    
+    var newRightBound = middleIndex - 1
+    rootOfLeftTree = BSTFromSortedArray(inputList, leftBound, newRightBound)
     currentRoot.lc = rootOfLeftTree 
   }
 
   // if there is a rightTree:
   if (rightBound > middleIndex) {
     console.log('a right tree exists!')
-    rootOfRightTree = BSTFromSortedArray(inputList, newLeftBound, newRightBound)  
+    
+    var newLeftBound = middleIndex + 1  
+    rootOfRightTree = BSTFromSortedArray(inputList, newLeftBound, rightBound)  
     currentRoot.rc = rootOfRightTree
   }
 
@@ -43,6 +47,7 @@ function createNodesFromValues(listOfValues) {
 
 // test
 var test = [1, 2, 3, 4, 5, 6]
-// var test = [1, 2, 3, 4, 5]
+var test = [1, 2, 3, 4, 5]
 
-BSTFromSortedArray(test)
+var rootOfTree = BSTFromSortedArray(test)
+console.log(rootOfTree)
