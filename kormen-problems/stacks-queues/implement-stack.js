0b9d72c0d69b55088cc287ced9c8c77b52e6f49e
ironpup <christoph.ding@gmail.com>
2017-09-05 20:59:12 -0400

    Finished implementing stack with array.

--- kormen-problems/stacks-queues/implement-stack.js
+++ kormen-problems/stacks-queues/implement-stack.js
@@ -1,21 +1,38 @@
 function stackWithArray() {
-  this.top = 0
+  this.nextToBeFilled = 0
   this.stack = []
 }
 
 stackWithArray.prototype.push = function(val) {
-  this.stack[this.top] = val
-  this.top = this.top + 1
+  this.stack[this.nextToBeFilled] = val
+  this.nextToBeFilled = this.nextToBeFilled + 1
 }
 
 stackWithArray.prototype.isEmpty = function() {
-  return this.top === 0
+  return this.nextToBeFilled === 0
+}
+
+stackWithArray.prototype.peek = function() {
+  var indexOfHighestPlate = this.nextToBeFilled - 1
+  var topOfStack = this.stack[indexOfHighestPlate]
+  return topOfStack
+}
+
+stackWithArray.prototype.pop = function() {
+  if (this.isEmpty()) {
+    return "underflow"
+  } else {
+    var plateToPop = this.peek()
+    this.nextToBeFilled = this.nextToBeFilled - 1
+    return plateToPop
+  }
 }
 
 // tests
 var stack = new stackWithArray()
-console.log(stack.isEmpty())
+console.log('empty: ', stack.isEmpty())
 stack.push(1)
 stack.push(2)
-console.log(stack.isEmpty())
+console.log('peeking: ', stack.peek())
+console.log('empty: ', stack.isEmpty())
 console.log(stack)
