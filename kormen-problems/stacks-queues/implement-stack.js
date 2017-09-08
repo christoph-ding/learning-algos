8cb2516abbb39b700ad6d9f575444e3548d076f2
ironpup <christoph.ding@gmail.com>
2017-09-07 22:24:22 -0400

    Implment stack with linked-list and tests.

--- kormen-problems/stacks-queues/implement-stack.js
+++ kormen-problems/stacks-queues/implement-stack.js
@@ -31,13 +31,18 @@ StackWithArray.prototype.pop = function() {
 }
 
 // tests for array
-var stack = new StackWithArray()
-console.log('empty: ', stack.isEmpty())
-stack.push(1)
-stack.push(2)
-console.log('peeking: ', stack.peek())
-console.log('empty: ', stack.isEmpty())
-console.log(stack)
+
+// var stack = new StackWithArray()
+// console.log(stack.isEmpty() === true)
+// stack.push(1)
+// console.log(stack.isEmpty() === false)
+// stack.push(2)
+// console.log(stack.peek() === 2)
+// console.log(stack.pop() === 2)
+// console.log(stack.peek() === 1)
+// console.log(stack.isEmpty() === false)
+// console.log(stack.pop() === 1)
+// console.log(stack.isEmpty() === true)
 
 // implementing with a linkedList
 function LinkedList(head, tail) {
@@ -51,24 +56,63 @@ function LLNode(val, next) {
 }
 
 function StackWithLL() {
-
   this.LL = new LinkedList()
 }
 
 StackWithLL.prototype.isEmpty = function() {
-
+  return this.LL.head === null && this.LL.tail === null
 }
 
 StackWithLL.prototype.peek = function() {
-
+  if (this.isEmpty()) {
+    return 'underflow'
+  } else {
+    return this.LL.tail
+  }
 }
 
 StackWithLL.prototype.push = function(val) {
+  var newNode = new LLNode(val)
 
+  if (this.LL.head === null) {
+    this.LL.head = newNode
+  } 
+
+  if (this.LL.tail === null) {
+    this.LL.tail = newNode
+  } else {
+    newNode.next = this.LL.tail
+    this.LL.tail = newNode
+  }
 }
 
 StackWithLL.prototype.pop = function() {
-
+  if (this.isEmpty()) {
+    return 'underflow'
+  } else {
+    var topPlate = this.peek()
+    this.LL.tail = this.LL.tail.next
+    return topPlate
+  }
 }
 
 // tests for Linked List
+var stackOne = new StackWithLL()
+console.log(stackOne.isEmpty()) // true
+console.log(stackOne.peek()) // underflow
+stackOne.push(1)
+stackOne.push(2)
+stackOne.push(3)
+traverseStack(stackOne)
+console.log(stackOne.pop())
+console.log(stackOne.pop())
+traverseStack(stackOne)
+
+function traverseStack(stack) {
+  var currentPlate = stack.peek()
+  while (currentPlate) {
+    console.log(currentPlate.val)
+    currentPlate = currentPlate.next
+  }
+}
+
