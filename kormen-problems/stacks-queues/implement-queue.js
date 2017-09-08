028fb2320628ebe8ead10faca0b370622e1c8433
ironpup <christoph.ding@gmail.com>
2017-09-08 17:48:52 -0400

    Start implementing queue with array.

---
+++ kormen-problems/stacks-queues/implement-queue.js
@@ -0,0 +1,19 @@
+function QueueWithArray() {
+
+}
+
+QueueWithArray.prototype.isEmpty = function() {
+
+}
+
+QueueWithArray.prototype.willOverFlow = function() {
+
+}
+
+QueueWithArray.prototype.enqueue = function() {
+
+}
+
+QueueWithArray.prototype.dequeue = function() {
+  
+}
