209515e6cd76e9f07e3c0fccba4ac85d91fcf75f
ironpup <christoph.ding@gmail.com>
2017-09-10 11:49:22 -0400

    Continue working on implementing queue with an array that loops around.

--- kormen-problems/stacks-queues/implement-queue.js
+++ kormen-problems/stacks-queues/implement-queue.js
@@ -1,12 +1,32 @@
 function QueueWithArray() {
-
+  this.front = 0
+  this.rear = 0
+  // we initialize the array to an arbitrary number
+  var arrayInitialSize = 10
+  this.array = Array(arrayInitialSize)
 }
 
 QueueWithArray.prototype.isEmpty = function() {
+  return this.front === this.rear
+}
 
+
+QueueWithArray.prototype.shouldLoop(marker) {
+  return marker === this.array.length - 1
 }
 
 QueueWithArray.prototype.willOverFlow = function() {
+  // 2 cases i can think of immediately ... 
+    // rear is to the 'left' of front
+    // [4, 5, tail, head, 2, 3] <-- it has 'looped around'
+    // tail = head - 1
+    // rear is to the 'right' of front
+    // [head, 2, 3, 4, tail] 
+    // tail = array.length - 1
+
+
+
+
 
 }
 
@@ -17,3 +37,8 @@ QueueWithArray.prototype.enqueue = function() {
 QueueWithArray.prototype.dequeue = function() {
   
 }
+
+// tests
+var queueOne = new QueueWithArray()
+console.log(queueOne)
+console.log(queueOne.isEmpty())
