0ca51f08bea2f19bd393bf6e1a934513ef864321
ironpup <christoph.ding@gmail.com>
2017-09-14 17:16:57 -0400

    Start min-stack.

---
+++ kormen-problems/stacks-queues/min-stack.js
@@ -0,0 +1,26 @@
+function minStack() {
+
+}
+
+minStack.prototype.isEmpty = function() {
+
+}
+
+minStack.prototype.peek = function() {
+
+}
+
+minStack.prototype.push = function(val) {
+
+}
+
+minStack.prototype.pop = function() {
+
+}
+
+minStack.prototype.getMin = function() {
+
+}
+// tests
+var inputOne = [1, 2, 3]
+var minStackOne = new minStack()
