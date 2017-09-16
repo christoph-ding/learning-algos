84c605d2ce96bfa92696a0cc6f3393d9c7c23ec2
ironpup <christoph.ding@gmail.com>
2017-09-15 21:36:47 -0400

    Continue to work and test min-stack.

--- kormen-problems/stacks-queues/min-stack.js
+++ kormen-problems/stacks-queues/min-stack.js
@@ -1,13 +1,16 @@
 function minStack() {
-
+  this.top = 1
+  this.plates = new Array(2)
 }
 
 minStack.prototype.isEmpty = function() {
-
+  return this.top === 1
 }
 
 minStack.prototype.peek = function() {
-
+  if (this.isEmpty()) {
+    return "underflow"
+  }
 }
 
 minStack.prototype.push = function(val) {
@@ -24,3 +27,7 @@ minStack.prototype.getMin = function() {
 // tests
 var inputOne = [1, 2, 3]
 var minStackOne = new minStack()
+console.log(minStackOne.isEmpty()) // true
+console.log(minStackOne.peek()) // underflow
+
+console.log(minStackOne)
