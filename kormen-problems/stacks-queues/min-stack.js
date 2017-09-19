bfd27092555856a59a53ecab62603d2f2cb18f6f
ironpup <christoph.ding@gmail.com>
2017-09-18 22:03:14 -0400

    Implement and test min stack.

--- kormen-problems/stacks-queues/min-stack.js
+++ kormen-problems/stacks-queues/min-stack.js
@@ -10,11 +10,26 @@ minStack.prototype.isEmpty = function() {
 minStack.prototype.peek = function() {
   if (this.isEmpty()) {
     return "underflow"
+  } else {
+    var topPlate = this.plates[this.top - 1]
+    return topPlate
   }
 }
 
 minStack.prototype.push = function(val) {
+  var newMin = null;  
 
+  if (this.isEmpty()) {
+    newMin = val      
+  } else {
+    currentMin = this.getMin()
+    newMin = Math.min(currentMin, val)
+  }
+  var newPlate = {'val': val, 'min': newMin}
+
+  this.plates[this.top] = newPlate
+  this.top = this.top + 1
+  return
 }
 
 minStack.prototype.pop = function() {
@@ -22,12 +37,16 @@ minStack.prototype.pop = function() {
 }
 
 minStack.prototype.getMin = function() {
-
+  if (this.isEmpty()) {
+    return 'underflow'
+  } else {    
+    return this.peek().min
+  }
 }
 // tests
 var inputOne = [1, 2, 3]
 var minStackOne = new minStack()
-console.log(minStackOne.isEmpty()) // true
-console.log(minStackOne.peek()) // underflow
-
+minStackOne.push(2)
+minStackOne.push(3)
+minStackOne.push(1)
 console.log(minStackOne)
