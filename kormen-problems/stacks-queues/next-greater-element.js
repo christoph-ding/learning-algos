68d3f2646f2d1bf750bad774b47b8b1961671e1c
ironpup <christoph.ding@gmail.com>
2017-09-19 22:26:40 -0400

    Continue next-greater-element.

--- kormen-problems/stacks-queues/next-greater-element.js
+++ kormen-problems/stacks-queues/next-greater-element.js
@@ -1,15 +1,25 @@
 function nextGreaterElement (inputList) {
+  // this is the 
   var helperStack = []
-  var output = []
+  var output = new Array(inputList.length)
 
-  function check(num) {
-    console.log(num)
-  }
-
-  inputList.forEach(function(num) {
-    check(num)
+  inputList.forEach(function(currentElem, indx) {
+    var nextElem = inputList[indx + 1]
+    if (nextElem > currentElem) {
+      output[indx] = nextElem
+    } else {
+      var helperPlate = {'elem': currentElem, 'indx': indx}
+      helperStack.push(helperPlate)
+    }
   })
 
+  for (var i = 0; i < output.length; i++) {
+    if (output[i] === undefined) {
+      output[i] = -1
+    }
+  }
+
+  console.log(helperStack)
   return output
 }
 
