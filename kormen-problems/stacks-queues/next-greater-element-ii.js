8c1d51a2e2958924493240d7b4bf4b435ac6c00a
ironpup <christoph.ding@gmail.com>
2017-09-21 22:47:54 -0400

    Begin next-greater-element-ii.

---
+++ kormen-problems/stacks-queues/next-greater-element-ii.js
@@ -0,0 +1,11 @@
+function nextGreaterElement (inputList) {
+  let output = []
+
+
+
+  return output
+}
+
+// tests
+let inputOne = [1, 2, 1]
+console.log(nextGreaterElement(inputOne)) // [2, -1, 2]
