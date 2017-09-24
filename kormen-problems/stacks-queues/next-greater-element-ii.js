e72c2ae1ddf4595a1117c7f419c72d72c4f5c3ae
ironpup <christoph.ding@gmail.com>
2017-09-24 15:01:26 -0400

    continue on next-great-element-ii.

--- kormen-problems/stacks-queues/next-greater-element-ii.js
+++ kormen-problems/stacks-queues/next-greater-element-ii.js
@@ -1,7 +1,19 @@
 function nextGreaterElement (inputList) {
   let output = []
+  let helperStack = []
 
+  inputList.forEach(function(num, indx) {
+    while (helperStack.length > 0) {
+      let stackTopIndex = helperStack.length - 1
+      let topPlace = helperStack[stackTopIndex]
+      if (num > topPlace.val) {
 
+      } else {
+        
+      }
+    }
+
+  })
 
   return output
 }
