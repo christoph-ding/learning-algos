691b3f171b57f36cf60666742bfab302d476a7c0
ironpup <christoph.ding@gmail.com>
2017-09-25 18:45:36 -0400

    Start implementing a stack with queues - both efficient pop and efficient push

---
+++ kormen-problems/stacks-queues/stacks-queues.js
@@ -0,0 +1,48 @@
+// efficient pop
+function stackEfficientPop() {
+
+}
+
+stackEfficientPop.prototype.isEmpty() {
+
+}
+
+stackEfficientPop.prototype.peek() {
+  
+}
+
+stackEfficientPop.prototype.push(val) {
+  
+}
+
+stackEfficientPop.prototype.pop() {
+  
+}
+
+// tests
+
+
+
+
+// efficient push
+function stackEfficientPush() {
+
+}
+
+stackEfficientPush.prototype.isEmpty() {
+
+}
+
+stackEfficientPush.prototype.peek() {
+  
+}
+
+stackEfficientPush.prototype.push(val) {
+  
+}
+
+stackEfficientPush.prototype.pop() {
+  
+}
+
+// tests
