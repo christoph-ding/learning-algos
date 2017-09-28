de177ea05e6f353578d5c2b10014bcb5cfb67cb8
ironpup <christoph.ding@gmail.com>
2017-09-27 22:38:26 -0400

    Started implementing stack with queues with a constant time pop.

--- kormen-problems/stacks-queues/stacks-queues.js
+++ kormen-problems/stacks-queues/stacks-queues.js
@@ -15,25 +15,41 @@ Queue.prototype.dequeue = function() {
   return this.q.shift()
 }
 
+Queue.prototype.peek = function() {
+  if (this.isEmpty()) {
+    return 'underflow'
+  } else {
+    return this.q[0]
+  }  
+}
+
 Queue.prototype.isOnlyTop = function() {
   return this.q.length === 1
 }
 
 // efficient pop
 function StackEfficientPop() {
-
+  this.top = new Queue()
+  this.restQ = new Queue()
 }
 
 StackEfficientPop.prototype.isEmpty = function () {
-
+  return this.top.isEmpty()
 }
 
 StackEfficientPop.prototype.peek = function () {
-  
+  return this.top.peek()
 }
 
 StackEfficientPop.prototype.push = function (val) {
-  
+  if (!(this.top.isEmpty())) {
+    while (this.restQ.length > 0) {
+      this.top.push(this.restQ.dequeue())
+    }
+    this.restQ = this.top
+    this.top = new Queue()    
+  }
+    this.top.enqueue(val)
 }
 
 StackEfficientPop.prototype.pop = function () {
@@ -41,6 +57,20 @@ StackEfficientPop.prototype.pop = function () {
 }
 
 // tests
+let stackOne = new StackEfficientPop()
+console.log(stackOne.isEmpty()) // true
+console.log(stackOne.peek()) // underflow
+stackOne.push(1)
+
+console.log(stackOne.isEmpty()) // false
+console.log(stackOne.peek()) // 1
+
+stackOne.push(2)
+stackOne.push(3)
+console.log(stackOne.isEmpty()) // false
+console.log(stackOne.peek()) // 3
+console.log(stackOne)
+
 
 // efficient push
 function StackEfficientPush() {
@@ -76,13 +106,13 @@ StackEfficientPush.prototype.pop = function () {
 }
 
 // tests
-let stackOne = new StackEfficientPush()
-console.log(stackOne.pop(1)) // underflow
-console.log(stackOne.isEmpty()) // true
-stackOne.push(1)
-stackOne.push(2)
-console.log(stackOne.isEmpty()) // false
-stackOne.push(3)
-console.log(stackOne.pop()) // 3
-console.log(stackOne.pop()) // 2
-console.log(stackOne)
+// let stackOne = new StackEfficientPush()
+// console.log(stackOne.pop(1)) // underflow
+// console.log(stackOne.isEmpty()) // true
+// stackOne.push(1)
+// stackOne.push(2)
+// console.log(stackOne.isEmpty()) // false
+// stackOne.push(3)
+// console.log(stackOne.pop()) // 3
+// console.log(stackOne.pop()) // 2
+// console.log(stackOne)
