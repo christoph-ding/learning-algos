c7b7cd2b5f557aa39c9f77c74f4bad71dbc353d6
ironpup <christoph.ding@gmail.com>
2017-09-28 22:17:43 -0400

    Start implementing decode string (without using recursion).

---
+++ kormen-problems/stacks-queues/decode_string.js
@@ -0,0 +1,20 @@
+function decode_stringWithoutRecursion (inputString) {
+  let output = ''
+
+  // iterate through the input 
+    // when you encounter an open [ 
+    // get "the thing inside" UNTIL the close bracket
+    // * n "the thing inside" and add to output
+
+
+
+  return output
+}
+
+// tests
+let inputStringOne = "3[a]2[bc]"
+console.log(decode_string(inputStringOne)) // "aaabcbc"
+
+function decode_stringUsingRecursion (inputString) {
+  
+}
