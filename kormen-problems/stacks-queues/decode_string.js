0a0faef49336e762bd31c1b65fb6c537e1ed288e
ironpup <christoph.ding@gmail.com>
2017-10-01 15:44:34 -0400

    Continue on refactor of decode_string to use array and join, instead of string concatenation.

--- kormen-problems/stacks-queues/decode_string.js
+++ kormen-problems/stacks-queues/decode_string.js
@@ -1,6 +1,6 @@
 // without recursion
 function decode_stringWithoutRecursion (inputString) {
-  let output = ''
+  let output = []
 
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
                     'h', 'i', 'j', 'k', 'l', 'm', 'n',
@@ -12,42 +12,10 @@ function decode_stringWithoutRecursion (inputString) {
 
   // we are imitating stacks using an array
   let characterStack = []
-  let currentSubString = []
 
-  function moveToOutput() {
-    currentSubString.forEach(function(char) {
-      output.push(char)
-    })
-    currentSubString = []
-  }
-
-  function multiplyString(multiplier) {
-    let counter = 1
-    let newSubString = []
-    let currentSubStringAsString = currentSubString.join('')
-
-    while (counter <= multiplier) {
-      newSubString.push(currentSubStringAsString)
-      counter = counter + 1
-    }
-    currentSubString = newSubString
-  }
-
-  function rewind() {
-    while (true) {
-      let currentCharacter = characterStack.pop()
-      if (alphabetSet.has(currentCharacter)) {
-        currentSubString.push(currentCharacter)
-      } else if ((!(isNan(currentCharacter)))) {
-        multiplyString(currentCharacter)
-
-        if (characterStack.length() === 0) {
-          moveToOutput()
-        }
-      }
-    }
-    console.log(characterStack)
-  }
+  // currentSubstring is actually also a stack, because we add characters to it 'in reverse order'
+  // of their occurent in inputString, because we rewind 'in reverse order'
+  let currentSubStringCharacters = []
 
   inputString.split('').forEach(function(char) {
     if (char === ']') {
@@ -57,13 +25,52 @@ function decode_stringWithoutRecursion (inputString) {
     }
   })
 
-  return output
+  return output.join('')
+
+  // helpers
+  function rewind() {
+    while (true) {
+      let currentCharacter = characterStack.pop()
+      if (alphabetSet.has(currentCharacter)) {
+        currentSubStringCharacters.push(currentCharacter)
+      } else if ((!(isNaN(currentCharacter)))) {
+        multiplyString(currentCharacter)
+        if (characterStack.length === 0) {
+          moveToOutput()
+          break
+        }
+      }
+    }
+  }
+
+  function multiplyString(multiplier) {
+
+    let counter = 1
+    let newSubString = []
+
+    while (counter <= multiplier) {
+      currentSubStringCharacters.forEach(function(char) {
+        newSubString.push(char)
+      })
+      counter = counter + 1
+    }
+
+    currentSubStringCharacters = newSubString
+  }
+  
+  function moveToOutput() {
+    while (currentSubStringCharacters.length) {
+      output.push(currentSubStringCharacters.pop())  
+    }
+  }
 }
 
 // tests
 let inputStringOne = "3[a]2[bc]"
-console.log(decode_stringWithoutRecursion(inputStringOne)) // "aaabcbc"
+// console.log(decode_stringWithoutRecursion(inputStringOne)) // "aaabcbc"
 
+let inputStringTwo = "3[a2[c]]2[bc]"
+console.log(decode_stringWithoutRecursion(inputStringTwo)) // "aaabcbc"
 
 // with recursion
 function decode_stringUsingRecursion (inputString) {
