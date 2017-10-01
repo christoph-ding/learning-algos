7ad1bf46f4627e3c26bc2b2f953598221052a362
ironpup <christoph.ding@gmail.com>
2017-10-01 15:17:01 -0400

    Refactor decode_string to use arrays to represent the characters in string, rather than string concatenation.

--- kormen-problems/stacks-queues/decode_string.js
+++ kormen-problems/stacks-queues/decode_string.js
@@ -2,19 +2,60 @@
 function decode_stringWithoutRecursion (inputString) {
   let output = ''
 
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+                    'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                    'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z'
+                   ]
+
+  const alphabetSet = new Set(alphabet)
+
   // we are imitating stacks using an array
   let characterStack = []
-  let currentSubString = ''
+  let currentSubString = []
+
+  function moveToOutput() {
+    currentSubString.forEach(function(char) {
+      output.push(char)
+    })
+    currentSubString = []
+  }
+
+  function multiplyString(multiplier) {
+    let counter = 1
+    let newSubString = []
+    let currentSubStringAsString = currentSubString.join('')
+
+    while (counter <= multiplier) {
+      newSubString.push(currentSubStringAsString)
+      counter = counter + 1
+    }
+    currentSubString = newSubString
+  }
+
+  function rewind() {
+    while (true) {
+      let currentCharacter = characterStack.pop()
+      if (alphabetSet.has(currentCharacter)) {
+        currentSubString.push(currentCharacter)
+      } else if ((!(isNan(currentCharacter)))) {
+        multiplyString(currentCharacter)
+
+        if (characterStack.length() === 0) {
+          moveToOutput()
+        }
+      }
+    }
+    console.log(characterStack)
+  }
 
   inputString.split('').forEach(function(char) {
     if (char === ']') {
-      console.log('rewind')
-      // 'rewind'
+      rewind()
     } else {
       characterStack.push(char)
     }
   })
-
 
   return output
 }
