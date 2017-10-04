7affb24aab0f60db34ad6b937cb8924fdd1971a6
ironpup <christoph.ding@gmail.com>
2017-10-03 21:54:48 -0400

    Finished merge-sort and tested.

--- kormen-problems/sort/merge-sort.js
+++ kormen-problems/sort/merge-sort.js
@@ -7,21 +7,59 @@ function mergeSort(inputList) {
     let midpointIndex = Math.floor(inputList.length / 2)
 
     let leftList = inputList.slice(0, midpointIndex)
-    console.log(leftList)
     let sortedLeft = mergeSort(leftList)
 
     let rightList = inputList.slice(midpointIndex)
-    console.log(rightList)
     let sortedRight = mergeSort(rightList)
 
-    return combineLists(sortedLeft, sortedRight)
+    let combinedSortedList = combineLists(sortedLeft, sortedRight)
+    return combinedSortedList
   }
 }
 
 function combineLists(listOne, listTwo) {
+  let indexOne = 0
+  let indexTwo = 0
 
+  let sortedCombined = []
+
+  while (indexOne < listOne.length && indexTwo < listTwo.length) {
+    let valOne = listOne[indexOne]
+    let valTwo = listTwo[indexTwo]
+
+    if (valOne < valTwo) {
+      sortedCombined.push(valOne)
+      indexOne = indexOne + 1
+    } else {
+      sortedCombined.push(valTwo)
+      indexTwo = indexTwo + 1
+    }
+  }
+
+  // put the 'rest' of the remaining list in
+  if (indexTwo > indexOne) {
+    while (indexOne < listOne.length) {
+      let valOne = listOne[indexOne]
+      sortedCombined.push(valOne)
+      indexOne = indexOne + 1
+    }
+  } else {
+    while (indexTwo < listTwo.length) {
+      let valTwo = listTwo[indexTwo]
+      sortedCombined.push(valTwo)
+      indexTwo = indexTwo + 1
+    }
+  }
+
+
+  return sortedCombined
 }
 
 // tests
 let inputOne = [3, 2, 4, 5, 1]
 console.log(mergeSort(inputOne))
+
+// let left = [3]
+// let right = [2]
+// console.log(combineLists(left, right))
+
