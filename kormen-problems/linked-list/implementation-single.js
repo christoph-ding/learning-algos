94485d76ac2a9496fc8c8f941795be280c3c9d41
ironpup <christoph.ding@gmail.com>
2017-10-07 14:54:51 -0400

    Stub out functions necceary to implement a Linked List and Linked List node, using a search + cb system.

---
+++ kormen-problems/linked-list/implementation-single.js
@@ -0,0 +1,41 @@
+function singleLinkedNode(key) {
+  this.key = key
+  this.next = null
+}
+
+singleLinkedNode.prototype.delete() {
+
+}
+
+singleLinkedNode.prototype.insertAfterTail(node) {
+
+}
+
+// no circular
+function singleLinkedList() {
+
+}
+
+singleLinkedList.prototype.isEmpty() {
+
+}
+
+singleLinkedList.prototype.searchForNode(key, cb) {
+
+}
+
+singleLinkedList.prototype.deleteNode(key) {
+
+}
+
+singleLinkedList.prototype.insertNodeAfterK(key) {
+
+}
+
+singleLinkedList.prototype.insertAfterTail(key) {
+
+}
+
+singleLinkedList.prototype.insertBeforeHead(key) {
+  
+}
