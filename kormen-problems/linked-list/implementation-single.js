7744c520866b075721bfe61a148b87ec03e2fa75
ironpup <christoph.ding@gmail.com>
2017-10-07 15:51:53 -0400

    Refactor the linked list to be more modular and usecallbacks, but it is a little awkward.

--- kormen-problems/linked-list/implementation-single.js
+++ kormen-problems/linked-list/implementation-single.js
@@ -10,15 +10,42 @@ singleLinkedNode.prototype.insertAfterTail = function(node) {
 // no circular
 function singleLinkedList(head) {
   this.head = head || null
-  this.tail = head || null
+  this.tail = this.findTail()
 }
 
-singleLinkedList.prototype.isEmpty = function() {
+singleLinkedList.prototype.iterateThroughLL = function(searchCondition, cb) {
+    var cb = cb || undefined
 
+    let currentNode = this.head
+
+    while (currentNode !== null) {
+      let foundNode = searchCondition(currentNode)
+      if (foundNode) {
+        if (cb) {
+          cb(foundNode)
+        }
+        break
+      }
+      currentNode = currentNode.next
+    }
+
+    return currentNode
 }
 
-singleLinkedList.prototype.searchForNode = function(key, cb) {
+singleLinkedList.prototype.findTail = function() {
+  function isTail(node) {
+    return node.next === null
+  }
 
+  return this.iterateThroughLL(isTail)
+}
+
+singleLinkedList.prototype.searchForNode = function(target) {
+  function isTarget(node) {
+    return node.key === target
+  }
+
+  return this.iterateThroughLL(isTarget)
 }
 
 singleLinkedList.prototype.deleteNode = function(key) {
@@ -38,10 +65,18 @@ singleLinkedList.prototype.insertBeforeHead = function(key) {
 }
 
 // tests
-var nodeOne = new singleLinkedNode(1)
-var nodeTwo = new singleLinkedNode(2)
-var nodeThree = new singleLinkedNode(3)
+let nodeOne = new singleLinkedNode(1)
+let nodeTwo = new singleLinkedNode(2)
+let nodeThree = new singleLinkedNode(3)
 nodeOne.insertAfterTail(nodeTwo)
 nodeTwo.insertAfterTail(nodeThree)
-console.log(nodeOne)
+
+// let listEmpty = new singleLinkedList()
+// console.log(listEmpty)
+
+let listOne = new singleLinkedList(nodeOne)
+console.log(listOne.searchForNode(3)) // finds
+console.log(listOne.searchForNode(4)) // does not find
+console.log(listOne)
+
 
