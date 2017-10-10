e21008dbbce94180ffe1c0ed8894dc3f41199a49
ironpup <christoph.ding@gmail.com>
2017-10-09 23:07:19 -0400

    Implemented and tested reverse linked-list recursively.

--- kormen-problems/linked-list/reverse-linked-list-recurse.js
+++ kormen-problems/linked-list/reverse-linked-list-recurse.js
@@ -1,24 +1,40 @@
+// node functions
 function linkedListNode (key) {
   this.key = key 
   this.next = null
 }
 
-
 function logAllLLNode(head) {
   let currentNode = head
+
+  let output = []
+
   while (currentNode !== null) {
-    console.log(currentNode.key)
+    output.push(currentNode.key)
     currentNode = currentNode.next
+  }
+
+  return output.join(' -> ')
+}
+
+// reverse linked list
+function reverseLinkedList(head) {
+  if (head.next !== null) {
+    reverseLinkedList(head.next)
+    head.next.next = head
+    head.next = null
   }
 }
 
 // tests
 let nodeOne = new linkedListNode(1) 
-let currentNode = nodeOne
-for (var i = 2; i < 7; i++) {
-  let newNode = new linkedListNode(i)
-  currentNode.next = newNode
-  currentNode = newNode
-}
+let nodeTwo = new linkedListNode(2)
+let nodeThree = new linkedListNode(3)
+let nodeFour = new linkedListNode(4)
+nodeThree.next = nodeFour
+nodeTwo.next = nodeThree
+nodeOne.next = nodeTwo
 
-logAllLLNode(nodeOne)
+console.log(logAllLLNode(nodeOne))
+reverseLinkedList(nodeOne)
+console.log(logAllLLNode(nodeFour))
