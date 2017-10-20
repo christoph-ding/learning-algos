17a63a58fbf20b5f8802cf9a430b89a2eb7c0916
ironpup <christoph.ding@gmail.com>
2017-10-19 22:39:46 -0400

    Start implementing palindrome linked list checker.

---
+++ kormen-problems/linked-list/palindrome-linked-list.js
@@ -0,0 +1,43 @@
+// node functions
+function linkedListNode (key) {
+  this.key = key 
+  this.next = null
+}
+
+function logAllLLNode(head) {
+  let currentNode = head
+  let output = []
+
+  while (currentNode !== null) {
+    output.push(currentNode.key)
+    currentNode = currentNode.next
+  }
+
+  return output.join(' -> ')
+}
+
+// 
+function palindromeChecker(head) {
+
+}
+
+
+// tests
+let nodeOne = new linkedListNode(1) 
+let nodeTwo = new linkedListNode(2)
+let nodeThree = new linkedListNode(3)
+let nodeFour = new linkedListNode(4)
+nodeThree.next = nodeFour
+nodeTwo.next = nodeThree
+nodeOne.next = nodeTwo
+
+let nodeOneB = new linkedListNode(5) 
+let nodeTwoB = new linkedListNode(6)
+let nodeThreeB = new linkedListNode(7)
+let nodeFourB = new linkedListNode(8)
+nodeThreeB.next = nodeFourB
+nodeTwoB.next = nodeThreeB
+nodeOneB.next = nodeTwoB
+
+console.log(logAllLLNode(nodeOne))
+console.log(logAllLLNode(nodeOneB))
