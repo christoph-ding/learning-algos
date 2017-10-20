a3ea7a46413b4518b341c4974e9a32bd02b8c5de
ironpup <christoph.ding@gmail.com>
2017-10-19 22:55:33 -0400

    Implemented the variable assignment to function calls.

--- kormen-problems/linked-list/palindrome-linked-list.js
+++ kormen-problems/linked-list/palindrome-linked-list.js
@@ -19,35 +19,60 @@ function logAllLLNode(head) {
 // 
 function palindromeChecker(head) {
 
-  // runners declared here?
-
   // find length 
+  const length = findLLSize(head)
 
   // use length to have access to 'midpoint node'
     // is it even length?
     // if it is odd length -> the 'middle is a gimme'
-    // ---> we have some 'mindpoint node'
+    // ---> we have some 'node that the second half starts at
+  const secondLLOrigHead = findSecondHalf(head, length)
 
   // reverse all after the midpoint node... 
     // so ... LL2 is the LL with head as mindpoint node
+  const secondLLReversedHead = reverseLL(secondLLOrigHead)
     
   // have 1 runner at the head of 'LL1'
   // have 1 runner at the 'head' of 'LL2'
+  let rOne = head
+  let rTwo = secondLLReversedHead
 
+  const secondHalfEqualToFirst = areLLEqual(rOne, rTwo)
+
+  if (secondHalfEqualToFirst) {
+    return true
+  } else {
+    return false
+  }
+
+}
+
+// helper functions
+function findLLSize(head) {
+
+  // integer
+}
+
+function findSecondHalf(head, length) {
+
+  // node
+}
+
+function reverseLL(head) {
+
+  // node
+}
+
+function areLLEqual(headOne, headTwo) {
   // iterate over 'both' LL1 and LL2 using runners
     // we don't actually care about 'stopping' at null, just that:
     // ? runner 1 == runner 2:
       // y: w.e
       // n: (break) -> false
-
-
+      
   // to have 'made it past the loop' -> true
-
+  // boolean
 }
-
-// helper functions
-
-
 
 // tests
 let nodeOne = new linkedListNode(1) 
