cb1e6087469350dd38cc29c838f473aa614fd960
ironpup <christoph.ding@gmail.com>
2017-10-24 22:43:56 -0400

    Implemented and tested productofArrayExceptSelf.js

--- randomProblems/productOfArrayExceptSelf.js
+++ randomProblems/productOfArrayExceptSelf.js
@@ -2,15 +2,32 @@
 function productOfArrayExceptSelf(nums) {
   const output = []
 
-  nums.forEach(function(num) {
-    console.log(num)
+  let productIncludingSelfandLeft = 1
+
+  nums.forEach(function(num, index) {
+    productIncludingSelfandLeft = productIncludingSelfandLeft * num
+    output[index] = productIncludingSelfandLeft 
   })
 
   // interate through the right, setting output to the final 
+  let productToRight = 1
+
+  for (let i = output.length - 1; i >= 0; i--) {
+    let firstIterationOutputVal = output[i]
+    let productOfAllToLeft;
+    if (i === 0) {
+      productToLeft = 1
+    } else {
+      productToLeft = output[i - 1]
+    }
+
+    let finalOutputVal = productToLeft * productToRight
+    productToRight = productToRight * nums[i]
+  }
 
   return output
 }
 
 // tests
 const listOne = [1,2,3,4]
-productOfArrayExceptSelf(listOne)
+console.log(productOfArrayExceptSelf(listOne))
