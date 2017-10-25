5b8efbdbce0a6b75141710b9e7ce5ddcb48dd339
ironpup <christoph.ding@gmail.com>
2017-10-24 22:31:31 -0400

    Start productOfArrayExceptSelf.

--- kormen-problems/linked-list/mergeSortTwoLists.js
+++ kormen-problems/linked-list/mergeSortTwoLists.js
@@ -20,10 +20,33 @@ function logAllLLNode(head) {
 
 function mergeTwoLists(headOne, headTwo) {
   // instantiate variables / runners
+  let sortedHead = null
+  let sortedTail = null
+  let runnerOne = headOne
+  let runnerTwo = headTwo
+
+
+  // in case of null
+  if (headOne === null && headTwo !== null) {
+    sortedHead = headTwo
+  } 
+
+  if (headTwo === null && headOne !== null) {
+    sortedHead = headOne
+  }
 
   // find the first node
+  if (runnerOne.key < runnerTwo.key) {
+    sortedTail = runnerOne
+    runnerOne = runnerOne.next
+  } else {
+    sortedTail = runnerTwo
+    runnerTwo = runnerTwo.next
+  }
 
+  console.log(sortedTail)
   // until both runners are exhausted ...
+  while ()
     // compare both runners
     // set next of sortedTail to smaller runner, increment that runner
 
@@ -31,17 +54,22 @@ function mergeTwoLists(headOne, headTwo) {
 
 // tests
 let nodeOne = new linkedListNode(1) 
-let nodeTwo = new linkedListNode(2)
-let nodeThree = new linkedListNode(3)
-let nodeFour = new linkedListNode(4)
+let nodeTwo = new linkedListNode(3)
+let nodeThree = new linkedListNode(5)
+let nodeFour = new linkedListNode(7)
 nodeThree.next = nodeFour
 nodeTwo.next = nodeThree
 nodeOne.next = nodeTwo
 
-let nodeOneB = new linkedListNode(5) 
-let nodeTwoB = new linkedListNode(6)
-let nodeThreeB = new linkedListNode(7)
+let nodeOneB = new linkedListNode(0) 
+let nodeTwoB = new linkedListNode(4)
+let nodeThreeB = new linkedListNode(6)
 let nodeFourB = new linkedListNode(8)
 nodeThreeB.next = nodeFourB
 nodeTwoB.next = nodeThreeB
 nodeOneB.next = nodeTwoB
+
+// console.log(logAllLLNode(nodeOne))
+// console.log(logAllLLNode(nodeOneB))
+
+mergeTwoLists(nodeOne, nodeOneB)
---
+++ randomProblems/productOfArrayExceptSelf.js
@@ -0,0 +1,16 @@
+// 
+function productOfArrayExceptSelf(nums) {
+  const output = []
+
+  nums.forEach(function(num) {
+    console.log(num)
+  })
+
+  // interate through the right, setting output to the final 
+
+  return output
+}
+
+// tests
+const listOne = [1,2,3,4]
+productOfArrayExceptSelf(listOne)
