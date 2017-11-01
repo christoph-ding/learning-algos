1221e162e75ec4e388a4e6aacac10f6c626e6762
ironpup <christoph.ding@gmail.com>
2017-10-31 22:24:26 -0400

    Implemented LLCycle.

--- algos-by-week/10.29.17/LL-helper.js
+++ algos-by-week/10.29.17/LL-helper.js
@@ -15,3 +15,20 @@ function logAllLLNode(head) {
 
   return output.join(' -> ')
 }
+
+// tests
+let nodeOne = new linkedListNode(1) 
+let nodeTwo = new linkedListNode(2)
+let nodeThree = new linkedListNode(3)
+let nodeFour = new linkedListNode(4)
+nodeThree.next = nodeFour
+nodeTwo.next = nodeThree
+nodeOne.next = nodeTwo
+
+let nodeOneB = new linkedListNode(5) 
+let nodeTwoB = new linkedListNode(6)
+let nodeThreeB = new linkedListNode(7)
+let nodeFourB = new linkedListNode(8)
+nodeThreeB.next = nodeFourB
+nodeTwoB.next = nodeThreeB
+nodeOneB.next = nodeTwoB
---
+++ algos-by-week/10.29.17/LLCycle.js
@@ -0,0 +1,51 @@
+function LLHasCycle(head) {
+  if (head === null || head.next === null) {
+    return false
+  }
+
+  let slow = head
+  let fast = head.next
+
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) {
+      return false
+    }
+    slow = slow.next
+    fast = fast.next.next
+  }
+  return true
+}
+
+// node functions
+function linkedListNode (key) {
+  this.key = key 
+  this.next = null
+}
+
+function logAllLLNode(head) {
+  let currentNode = head
+  let output = []
+
+  while (currentNode !== null) {
+    output.push(currentNode.key)
+    currentNode = currentNode.next
+  }
+
+  return output.join(' -> ')
+}
+
+// tests
+let nodeOne = new linkedListNode(1) 
+let nodeTwo = new linkedListNode(2)
+let nodeThree = new linkedListNode(3)
+let nodeFour = new linkedListNode(4)
+nodeThree.next = nodeFour
+nodeTwo.next = nodeThree
+nodeOne.next = nodeTwo
+
+// console.log(logAllLLNode(nodeOne))
+console.log(LLHasCycle(null))
+console.log(LLHasCycle(nodeFour))
+console.log(LLHasCycle(nodeOne))
+nodeFour.next = nodeOne
+console.log(LLHasCycle(nodeOne))
