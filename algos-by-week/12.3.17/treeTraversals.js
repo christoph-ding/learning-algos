a1475ee6e9b728f735ed54c02133964f16f869b7
ironpup <christoph.ding@gmail.com>
2017-12-05 21:34:19 -0500

    Began tree traversals.

---
+++ algos-by-week/12.3.17/treeTraversals.js
@@ -0,0 +1,12 @@
+function treeNode (val, lc, rc) {
+
+}
+
+function preOrderTraversal (root) {
+
+}
+
+// tests
+let three = treeNode('3')
+let two = treeNode('2', one, three)
+let one = treeNode('1')
