b8a045d53b20e7243d51a148204ae622976f173b
ironpup <christoph.ding@gmail.com>
2017-12-06 22:25:38 -0500

    Implement preorder, inorder and postorder traversal.

--- algos-by-week/12.3.17/treeTraversals.js
+++ algos-by-week/12.3.17/treeTraversals.js
@@ -1,12 +1,51 @@
 function treeNode (val, lc, rc) {
-
+  this.val = val
+  this.lc = lc || null
+  this.rc = rc || null
 }
 
 function preOrderTraversal (root) {
+  console.log(root.val)
+  if (root.lc) {
+    preOrderTraversal(root.lc)
+  } 
+  if (root.rc) {
+    preOrderTraversal(root.rc)
+  }
+}
 
+function inOrderTraversal (root) {
+  if (root.lc) {
+    inOrderTraversal(root.lc)
+  } 
+  console.log(root.val)
+  if (root.rc) {
+    inOrderTraversal(root.rc)
+  }
+}
+
+function postOrderTraversal (root) {
+  if (root.lc) {
+    postOrderTraversal(root.lc)
+  }
+  if (root.rc) {
+    postOrderTraversal(root.rc)
+  }
+  console.log(root.val)
 }
 
 // tests
-let three = treeNode('3')
-let two = treeNode('2', one, three)
-let one = treeNode('1')
+let five = new treeNode('5')
+let four = new treeNode('4', null, five)
+let three = new treeNode('3', null, four)
+let one = new treeNode('1')
+let two = new treeNode('2', one, three)
+
+console.log('pre order')
+preOrderTraversal(two)
+
+console.log('in order')
+inOrderTraversal(two)
+
+console.log('post order')
+postOrderTraversal(two)
