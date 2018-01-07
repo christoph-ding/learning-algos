ce67cd0b14d61fd4501f2ca7e443ed10bffb8d2c
ironpup <christoph.ding@gmail.com>
2018-01-07 17:24:22 -0500

    Begin rectangle overlap problem.

---
+++ algos-by-week/1.7.18/rectangleOverlap.js
@@ -0,0 +1,40 @@
+function rectangleOverlap (Arectangle, BRectangle) {
+  // declare variables
+  let horizontalOverlapExists = verticalOverlapExists = false
+  let horizontalOverlapSize = verticalOverlapSize = 0
+
+  // are there overlaps?
+  // horizontal
+  if () {
+
+  }
+
+  else if () {
+
+  }
+
+  // vertical
+  if () {
+
+  }
+
+  else if () {
+
+  }
+
+  // if so, return area
+  let overlapExists = (horizontalOverlapExists && verticalOverlapExists)
+
+  if (overlapExists) {
+    return horizontalOverlapSize * verticalOverlapSize
+  }
+
+  else {
+    return 0
+  }
+
+}
+
+// tests
+const ARectangleOne = [[0, 0], [10, 10]]
+const BRectangleTwo = [[5, 5], [15, 15]]
