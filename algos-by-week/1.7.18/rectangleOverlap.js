2eb30fee7d183a347a7c160a1f0a36fe9843f92d
ironpup <christoph.ding@gmail.com>
2018-01-11 22:44:25 -0500

    Implement + tested rectangle overlap.

--- algos-by-week/1.7.18/rectangleOverlap.js
+++ algos-by-week/1.7.18/rectangleOverlap.js
@@ -1,7 +1,7 @@
 function rectangleOverlap (Arectangle, Brectangle) {
   // declare variables
-  let horizontalOverlapExists = verticalOverlapExists = false
-  let horizontalOverlapSize = verticalOverlapSize = 0
+  // overlap is an integer that can be evaluated as a boolean
+  let horizontalOverlap = verticalOverlap = 0
 
   console.log('Arectangle', Arectangle)
   console.log('BRectangle', Brectangle)
@@ -16,50 +16,45 @@ function rectangleOverlap (Arectangle, Brectangle) {
   const BrcX = Brectangle[1][0]
   const BrcY = Brectangle[1][1]
 
-  console.log("AlcX: ", AlcX)
-  console.log("AlcY: ", AlcY)
-  console.log("ArcX: ", ArcX)
-  console.log("ArcY: ", ArcY)
-
-  console.log("BlcX: ", BlcX)
-  console.log("BlcY: ", BlcY)
-  console.log("BrcX: ", BrcX)
-  console.log("BrcY: ", BrcY)
-
   // are there overlaps?
   // horizontal
   if (BlcX < ArcX && BlcX > AlcX) {
-    horizontalOverlapExists = true
+    horizontalOverlap = absoluteDifference(BlcX, ArcX)
+  } else if (BrcX > AlcX && BrcX < ArcX) {
+    horizontalOverlapS = absoluteDifference(Brcx, Alcx)
   }
 
-  else if () {
-     
+  // vertical
+  if (BlcY < ArcY && BlcY > AlcY) {
+    verticalOverlap = absoluteDifference(BlcY, ArcY)
+  } else if (BrcY > AlcY && BrcY < ArcY) {
+    verticalOverlap = absoluteDifference(BrcY, AlcY)
   }
 
-  // // vertical
-  // if () {
+  // testing
+  console.log('horizontalOverlap: ', horizontalOverlap)
+  console.log('verticalOverlap: ', verticalOverlap)
 
-  // }
+  return horizontalOverlap * verticalOverlap
+}
 
-  // else if () { 
-
-  // }
-
-  // // if so, return area
-  // let overlapExists = (horizontalOverlapExists && verticalOverlapExists)
-
-  // if (overlapExists) {
-  //   return horizontalOverlapSize * verticalOverlapSize
-  // }
-
-  // else {
-  //   return 0
-  // }
-
+function absoluteDifference(a, b) {
+  return Math.abs(a - b)
 }
 
 // tests
-const ARectangle = [[0, 0], [10, 10]]
-const BRectangle = [[5, 5], [15, 15]]
+const ARectangleOne = [[0, 0], [10, 10]]
+const BRectangleOne = [[5, 5], [15, 15]]
+console.log(rectangleOverlap(ARectangleOne, BRectangleOne))
 
-console.log(rectangleOverlap(ARectangle, BRectangle))
+console.log("===============================")
+
+const ARectangleTwo = [[0, 0], [10, 10]]
+const BRectangleTwo = [[12, 12], [15, 15]]
+console.log(rectangleOverlap(ARectangleTwo, BRectangleTwo))
+
+console.log("===============================")
+
+const ARectangleThree = [[0, 0], [10, 10]]
+const BRectangleThree = [[7, 5], [10, 11]]
+console.log(rectangleOverlap(ARectangleThree, BRectangleThree))
