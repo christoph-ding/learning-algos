8834c991f268558c78d133c86356ccfbe1b9bd8f
ironpup <christoph.ding@gmail.com>
2018-01-15 22:06:24 -0500

    Implement and tested permutations.

--- algos-by-week/1.15.18/permutations.js
+++ algos-by-week/1.15.18/permutations.js
@@ -1,10 +1,26 @@
 function permutations(elements) {
-  let permutations = []
+  const permutations = []
+  let currentPerm = []
 
+  function inner(subElements) {
+    if (!(subElements.length)) {
+      let currentPermAsString = currentPerm.join('')
+      permutations.push(currentPermAsString))
+    } else {
+      subElements.forEach( (elem, index) => {
+        currentPerm.push(elem)
+        let otherElements = subElements.slice(0, index).concat(subElements.slice(index + 1))
+        inner(otherElements)
+        currentPerm.pop()
+      })
+    }
+  }
+
+  inner(elements)
 
   return permutations
 }
 
 // tests
-var testOne = ['a', 'b', 'c']
-console.log(permutations(testOne)
+ var testOne = [1, 2, 3]
+console.log(permutations(testOne))
