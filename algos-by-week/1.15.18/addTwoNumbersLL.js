10f43b33f1a55eaa92eac9af0beea113ae3c1244
ironpup <christoph.ding@gmail.com>
2018-01-16 21:16:31 -0500

    Continue the two linked list addition.

--- algos-by-week/1.15.18/addTwoNumbersLL.js
+++ algos-by-week/1.15.18/addTwoNumbersLL.js
@@ -4,14 +4,24 @@ function linkedListNode(val, next) {
 }
 
 function addTwoNumbersLL(numOne, numTwo) {
-  let currentTail  = null
+  let starterLL = new linkedListNode(0, null)
+  let currentTail = starterLL
   let currentOne = numOne || null
   let currentTwo = numTwo || null
   let carryOne = 0
 
   while (currentOne || currentTwo) {
-    let digitOne = currentOne.val || 0
-    let digitTwo = currentTwo.val || 0
+
+    let digitOne = 0
+    let digitTwo = 0
+    if (currentOne) {
+      digitOne = currentOne.val
+    } 
+
+    if (currentTwo) {
+      digitTwo = currentTwo.val
+    } 
+
     let newValue = digitOne + digitTwo + carryOne
 
     if (newValue >= 10) {
@@ -21,30 +31,45 @@ function addTwoNumbersLL(numOne, numTwo) {
       carryOne = 0
     }
 
-    let newDigitNode = new linkedListNode(newValue, null)
+    currentTail.next = new linkedListNode(newValue, null)
 
-    if (!(currentTail)) {
-      currentTail = newDigitNode
+    currentTail = currentTail.next
+
+    if (currentOne !== null) {
+      currentOne = currentOne.next  
     } else {
-      currentTail.next = newDigitNode
+      currentOne = null
     }
 
-    currentOne = currentOne.next || null
-    currentTwo = currentTwo.next || null
+    if (currentTwo !== null) {
+      currentTwo = currentTwo.next  
+    } else {
+      currentTwo = null
+    }
 
     console.log('adding: ', digitOne, ' + ', digitTwo, ' newValue: ', newValue, ' digit: ', newValue % 10, ' carryOne: ', carryOne)
   }
 
   // testing
+  let currentLogging = starterLL.next
+  console.log('=======================')
 
-
+  while (currentLogging) {
+    console.log(currentLogging)
+    currentLogging = currentLogging.next
+  }
 }
 
 let llThree = new linkedListNode(3, null)
 let llTwo = new linkedListNode(9, llThree)
 let llOne = new linkedListNode(5, llTwo)
 
-let llSix = new linkedListNode(2, null)
+
+// 5 -> 9 -> 3       4 -> 1 -> 2 -> 7
+// 395 + 7214
+
+let llSeven = new linkedListNode(7, null)
+let llSix = new linkedListNode(2, llSeven)
 let llFive = new linkedListNode(1, llSix)
 let llFour = new linkedListNode(4, llFive)
 
