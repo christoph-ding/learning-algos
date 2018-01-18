8249b26c648588c25bc6ad37c2ab3e0078799ad4
ironpup <christoph.ding@gmail.com>
2018-01-17 20:44:06 -0500

    Begin to implement mergesorting 2 linked lists.

---
+++ algos-by-week/1.15.18/MergeSortLL.js
@@ -0,0 +1,52 @@
+function llNode(val, next) {
+  this.val = val || 0
+  this.next = next || null
+}
+
+function mergeSort(head) {
+  // if head represents a single element list, just return head
+
+  // find the middle of the list,
+  // so that we can have a 'left' and 'right' list to recurse on
+
+  // mergeSort the left and the right
+  // so that we have 2 sorted lists
+
+  // merge both sorted lists
+
+  // return a sorted-list
+}
+
+function findMiddle(head) {
+  // fast and slow runners!
+
+}
+
+function mergeTwoSortedLists(headOne, headTwo) {
+
+}
+
+// tests
+let nodeFour = new llNode(4)
+let nodeThree = new llNode(3)
+let nodeTwo = new llNode(2)
+let nodeOne = new llNode(1)
+
+nodeOne.next = nodeThree
+nodeTwo.next = nodeFour
+
+function logAll(head) {
+  const listNodes = []
+
+  let currentNode = head
+
+  while (currentNode) {    
+    listNodes.push(currentNode.val)
+    currentNode = currentNode.next
+  }
+
+  return listNodes.join(' -> ')
+}
+
+console.log(logAll(nodeOne))
+console.log(logAll(nodeTwo))
