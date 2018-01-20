9c1b7678f5f99b81e222ea10a83a8fd314a2c855
ironpup <christoph.ding@gmail.com>
2018-01-20 17:25:16 -0500

    Implemented different tree traversals.

--- algos-by-week/1.15.18/treeImplement.js
+++ algos-by-week/1.15.18/treeImplement.js
@@ -5,21 +5,36 @@ function treeNode(val, lc, rc) {
 }
 
 function inOrder(root) {
-
+  if (root !== null) {
+    inOrder(root.lc)
+    console.log(root.val)
+    inOrder(root.rc)
+  }
 }
 
 function preOrder(root) {
-
+  if (root !== null) {
+    console.log(root.val)
+    preOrder(root.lc)
+    preOrder(root.rc)
+  }
 }
 
 function postOrder(root) {
-
+  if (root !== null) {
+    postOrder(root.rc)
+    postOrder(root.lc)
+    console(root.val)
+  }
 }
 
-let nodeSeven = (17, null, null)
-let nodeSix = (12, null, null)
-let nodeFix = (7, null, null)
-let nodeFour = (3, null, null)
-let nodeTwo = (5, nodeFour, nodeFive)
-let nodeThree = (15, nodeSix, nodeSeven)
-let nodeOne = (10, nodeTwo, nodeThree)
+let nodeSeven = new treeNode(17, null, null)
+let nodeSix = new treeNode(12, null, null)
+let nodeFive = new treeNode(7, null, null)
+let nodeFour = new treeNode(3, null, null)
+let nodeTwo = new treeNode(5, nodeFour, nodeFive)
+let nodeThree = new treeNode(15, nodeSix, nodeSeven)
+let nodeOne = new treeNode(10, nodeTwo, nodeThree)
+
+console.log('in order: ')
+inOrder(nodeOne)
