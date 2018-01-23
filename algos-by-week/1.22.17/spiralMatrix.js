372b9db6bfbc427d3a515c2318f4dcdeff791fb7
ironpup <christoph.ding@gmail.com>
2018-01-22 22:02:40 -0500

    Stub out steps for spiral matrix.

---
+++ algos-by-week/1.22.17/spiralMatrix.js
@@ -0,0 +1,39 @@
+function spiralMatrix(matrix) {
+  let xCoord = 0
+  let yCoord = 0
+
+  let boundRight
+  let boundUp
+  let boundDown
+  let boundLeft
+
+  while (stillGoing()) {
+    goLeft()
+    // increment
+    goDown()
+    // increment
+    goLeft()
+    // increment
+    goUp()
+    // increment
+  }
+
+  function goLeft() {
+
+  }
+
+  function goDown() {
+
+  }
+
+  function goLeft() {
+
+  }
+
+  function goUp() {
+
+  }
+}
+
+// tests
+
