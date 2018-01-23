0479feb00d3515e95ab30d3220775563768739a8
ironpup <christoph.ding@gmail.com>
2018-01-22 20:55:19 -0500

    Implemented BST insertion.

--- algos-by-week/1.22.17/implementBST.js
+++ algos-by-week/1.22.17/implementBST.js
@@ -5,23 +5,36 @@ function treeNode(val) {
   this.rc = null
 }
 
-treeNode.prototype.setLeft(node) {
+treeNode.prototype.setLeft = function(node) {
   this.lc = node
 }
 
-treeNode.prototype.setRight(node) {
+treeNode.prototype.setRight = function(node) {
   this.rc = node
 }
 
-treeNode.prototype.insertRight(node) {
-
+treeNode.prototype.insert = function(node) {
+  if (node) {
+    if (node.val < this.val) {
+      // node has children?
+      if (this.lc) {
+        this.lc.insert(node)
+      }
+      else if (!(this.lc)) {
+        this.lc = node
+      }
+    } else if (node.val > this.val) {
+      // node has children?
+      if (this.rc) {
+        this.rc.insert(node)
+      } else {
+        this.rc = node
+      }
+    }
+  }
 }
 
-treeNode.prototype.insertLeft(node) {
-
-}
-
-treeNode.prototype.traverseInOrder() {
+treeNode.prototype.traverseInOrder = function() {
 
 }
 
@@ -30,4 +43,15 @@ let nodeOne = new treeNode(15)
 let nodeTwo = new treeNode(8)
 let nodeThree = new treeNode(23)
 let nodeFour = new treeNode(5)
+let nodeFive = new treeNode(2)
+let nodeSix = new treeNode(29)
+let fakeNode = null
 
+// nodeOne.insert(fakeNode)
+// console.log(nodeOne) // should have no children
+nodeOne.insert(nodeTwo) 
+nodeOne.insert(nodeThree) 
+nodeOne.insert(nodeFour)
+nodeOne.insert(nodeFive)
+nodeOne.insert(nodeSix)
+console.log(nodeOne) // should have lc and rc
