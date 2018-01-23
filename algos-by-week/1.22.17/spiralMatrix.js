56e24af1d3e8a0ceaf27293fc0eb390d25681bdf
ironpup <christoph.ding@gmail.com>
2018-01-23 18:55:08 -0500

    Implement a rough solution for spiral matrix.  Must test more.

--- algos-by-week/1.22.17/spiralMatrix.js
+++ algos-by-week/1.22.17/spiralMatrix.js
@@ -2,38 +2,70 @@ function spiralMatrix(matrix) {
   let xCoord = 0
   let yCoord = 0
 
-  let boundRight
-  let boundUp
-  let boundDown
-  let boundLeft
+  let boundRight = Math.max(matrix[0].length - 1, 0)
+  let boundUp = 0
+  let boundDown = Math.max(matrix.length - 1, 0)
+  let boundLeft = 0
 
   while (stillGoing()) {
-    goLeft()
-    // increment
+    goRight()
+    boundUp = boundUp + 1
+
     goDown()
-    // increment
+    boundRight = boundRight - 1
+
     goLeft()
-    // increment
+    boundDown = boundDown - 1
+
     goUp()
-    // increment
+    boundLeft = boundLeft + 1
   }
 
-  function goLeft() {
+  console.log(matrix[boundUp][boundLeft])
 
+  function goRight() {
+    for (let x = boundLeft; x <= boundRight; x++) {
+      xCoord = x
+      action()
+    }
   }
 
   function goDown() {
-
+    for (let y = boundUp; y <= boundDown; y++) {
+      yCoord = y
+      action()
+    }
   }
 
   function goLeft() {
-
+    for (let x = boundRight; x >= boundLeft; x--) {
+      xCoord = x
+      action()
+    }
   }
 
   function goUp() {
+    for (let y = boundDown; y >= boundUp; y--) {
+      yCoord = y
+      action()
+    }
+  }
 
+  function stillGoing() {
+    return ((boundRight !== boundLeft) && (boundDown !== boundUp))
+  }
+
+  function action() {
+    let valueAtCoord = matrix[yCoord][xCoord]
+    console.log(valueAtCoord)
   }
 }
 
 // tests
+const matrixOne = [
+        [1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5]
+      ]
 
+spiralMatrix(matrixOne)
