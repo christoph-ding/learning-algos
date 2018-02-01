c23850c258d4d96f7e612d930686a11493da6a5b
ironpup <christoph.ding@gmail.com>
2018-01-31 22:33:05 -0500

    Continue on the extract method of binary heap.

--- algos-by-week/1.28.17/binaryHeap.js
+++ algos-by-week/1.28.17/binaryHeap.js
@@ -8,12 +8,59 @@ BinaryHeap.prototype.getMin = function() {
 }
 
 BinaryHeap.prototype.insert = function(val) {
-  console.log('insert: ', val)
   this.heap[this.size] = val
   this.percUp(this.size)
   this.size = this.size + 1
-  console.log('heap after: ', this.heap)
-  console.log('===================')
+  // console.log('insert: ', val, ' heap after: ', this.heap)
+  // console.log('===================')
+}
+
+BinaryHeap.prototype.extract = function() {
+  const minimum = this.getMin()
+
+  // make the last element the root
+  this.heap[0] = this.heap[this.size - 1]
+  this.heap[this.size - 1] = null
+  this.size = this.size - 1
+
+  // percDown
+  this.percDown()
+
+  return minimum
+}
+
+BinaryHeap.prototype.percDown = function() {
+  let parentIndx = 0
+  let parentValue = this.heap[parentIndx]
+
+  // which child is smaller?
+  let lcIndx = null
+  let lcValue = null
+  let rcIndx = null
+  let rcValue = null
+  let oneToUseIndx = null
+  let oneToUseValue = null
+
+  // 
+  while ()
+
+
+
+  function calc() {
+    lcIndx = parentIndx * 2 + 1
+    lcValue = this.heap[lcIndx]
+    rcIndx = parentIndx * 2 + 2
+    rcValue = this.heap[rcIndx]
+
+    if (!(rcValue) || lcValue < rcValue) {
+      oneToUseIndx = lcIndx
+      oneToUseValue = lcValue
+    } else {
+      oneToUseIndx = rcIndx
+      oneToUseValue = rcValue
+    }
+    console.log('lc: ', lcValue, ' rc: ', rcValue, ' use: ', oneToUseValue)
+  }
 }
 
 BinaryHeap.prototype.percUp = function(indx) {
@@ -39,10 +86,6 @@ BinaryHeap.prototype.percUp = function(indx) {
   } 
 }
 
-BinaryHeap.prototype.insert = function(val) {
-  let lastIndex = 
-  this.heap[this.size + 1]
-}
 
 let testBinaryHeap = new BinaryHeap
 testBinaryHeap.insert(30)
@@ -51,4 +94,7 @@ testBinaryHeap.insert(11)
 testBinaryHeap.insert(100)
 testBinaryHeap.insert(50)
 testBinaryHeap.insert(10)
-console.log('min: ', testBinaryHeap.getMin())
+testBinaryHeap.insert(7)
+console.log(testBinaryHeap)
+
+console.log('min: ', testBinaryHeap.extract())
