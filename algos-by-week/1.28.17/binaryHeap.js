192495f9540af5851b4f95d3b015e88d604aaa84
ironpup <christoph.ding@gmail.com>
2018-01-31 21:45:01 -0500

    Working commit.

--- algos-by-week/1.28.17/binaryHeap.js
+++ algos-by-week/1.28.17/binaryHeap.js
@@ -1,9 +1,15 @@
 function BinaryHeap() {
-  this.heap = [0, 1]
+  this.heap = [0]
+  this.size = 0
 }
 
 BinaryHeap.prototype.getMin = function() {
   return this.heap[0]
+}
+
+BinaryHeap.prototype.insert = function(val) {
+  let lastIndex = 
+  this.heap[this.size + 1]
 }
 
 let testBinaryHeap = new BinaryHeap
