d6effa33ed9d9856edad8eba29dd27f326064e1a
ironpup <christoph.ding@gmail.com>
2018-01-31 21:17:50 -0500

    Implement insert and percUp functions for binary heap.

--- algos-by-week/1.28.17/binaryHeap.js
+++ algos-by-week/1.28.17/binaryHeap.js
@@ -1,10 +1,49 @@
 function BinaryHeap() {
-  this.heap = [0, 1]
+  this.heap = []
+  this.size = 0
 }
 
 BinaryHeap.prototype.getMin = function() {
-  return this.heap[0]
+  return this.heap[0] || 'empty heap'
+}
+
+BinaryHeap.prototype.insert = function(val) {
+  console.log('insert: ', val)
+  this.heap[this.size] = val
+  this.percUp(this.size)
+  this.size = this.size + 1
+  console.log('heap after: ', this.heap)
+  console.log('===================')
+}
+
+BinaryHeap.prototype.percUp = function(indx) {
+  let childIndx = indx
+  let childValue = this.heap[childIndx]  
+  let parentIndx = Math.floor((indx - 1)/ 2)
+  let parentValue = this.heap[parentIndx]
+
+  while (childIndx > 0) {
+    if (childValue < parentValue) {
+      // swap
+      this.heap[childIndx] = parentValue
+      this.heap[parentIndx] = childValue
+
+      // update
+      childIndx = parentIndx
+      childValue = this.heap[childIndx]
+      parentIndx = Math.floor((childIndx - 1)/ 2)
+      parentValue = this.heap[parentIndx]
+    } else {
+      break
+    }
+  } 
 }
 
 let testBinaryHeap = new BinaryHeap
-console.log(testBinaryHeap.getMin())
+testBinaryHeap.insert(30)
+testBinaryHeap.insert(40)
+testBinaryHeap.insert(11)
+testBinaryHeap.insert(100)
+testBinaryHeap.insert(50)
+testBinaryHeap.insert(10)
+console.log('min: ', testBinaryHeap.getMin())
--- algos-by-week/1.28.17/zigZagTraversal.js
+++ algos-by-week/1.28.17/zigZagTraversal.js
@@ -1,5 +1,24 @@
-function zigZagTraversal () {
+function treeNode(val, lc, rc) {
+  this.val = val || null
+  this.lc = lc || null
+  this.rc = rc || null
+}
 
+function zigZagTraversal(root) {
+  const output = []
+  const currentLevel = [root]
+  const nextLevel = []
+  let zigRight = true
+
+  while (currentLevel.length) {
+    console.log('doing somthing ...')
+
+
+
+
+  }
 }
 
 // tests
+let nodeOne = new treeNode(5, null, null)
+zigZagTraversal(nodeOne)
