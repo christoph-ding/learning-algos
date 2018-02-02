89315a723ac0daf85b40f119d092989e9e1ca2d1
ironpup <christoph.ding@gmail.com>
2018-02-01 21:25:40 -0500

    Continue to work on zig Zag traversal, but I feel I made a mistake.

--- algos-by-week/1.28.17/zigZagTraversal.js
+++ algos-by-week/1.28.17/zigZagTraversal.js
@@ -6,19 +6,51 @@ function treeNode(val, lc, rc) {
 
 function zigZagTraversal(root) {
   const output = []
-  const currentLevel = [root]
-  const nextLevel = []
+  let currentLevel = [root]
+  let currentVals = []
+  let nextLevel = []
   let zigRight = true
 
   while (currentLevel.length) {
-    console.log('doing somthing ...')
+    for (let i = currentLevel.length - 1; i >= 0; i--) {
+      let currentNode = currentLevel[i]
 
+      console.log('currentNode: ', currentNode.val)
+      currentVals.push(currentNode.val)
 
-
-
+      if (zigRight) {
+        if (currentNode.rc) {
+          nextLevel.push(currentNode.rc)
+        }
+        if (currentNode.lc) {
+          nextLevel.push(currentNode.lc)
+        }
+      } else if (!(zigRight)) {
+        if (currentNode.lc) {
+          nextLevel.push(currentNode.lc)
+        }
+        if (currentNode.rc) {
+          nextLevel.push(currentNode.rc)
+        }
+      }
+    }
+    output.push(currentVals.slice())
+    currentVals = []
+    currentLevel = nextLevel
+    nextLevel = []
+    zigRight = !(zigRight)
+    console.log('current: ', currentLevel)
   }
+
+  return output
 }
 
 // tests
-let nodeOne = new treeNode(5, null, null)
-zigZagTraversal(nodeOne)
+let nodeSeven = new treeNode(7)
+let nodeSix = new treeNode(15)
+let nodeFive = new treeNode(2)
+let nodeFour = new treeNode(1)
+let nodeThree = new treeNode(20, nodeSix, nodeSeven)
+let nodeTwo = new treeNode(9, nodeFour, nodeFive)
+let nodeOne = new treeNode(3, nodeTwo, nodeThree)
+console.log(zigZagTraversal(nodeOne))
