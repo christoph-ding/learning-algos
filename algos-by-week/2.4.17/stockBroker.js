803cb6f7e2c4c45488e676487921ab584a3563b0
ironpup <christoph.ding@gmail.com>
2018-02-05 21:00:03 -0500

    Begin stock broker.

---
+++ algos-by-week/2.4.17/stockBroker.js
@@ -0,0 +1,14 @@
+function stockBroker(pricesByDay) {
+  let lowestPriceSoFar = null
+  let maxProfitSoFar = 0
+
+  pricesByDay.forEach(function(dailyPrice) {
+
+  })
+
+  return maxProfitSoFar
+}
+
+// tests
+var pricesOne = [3, 2, 1, 0]
+console.log(stockBroker(pricesOne))
