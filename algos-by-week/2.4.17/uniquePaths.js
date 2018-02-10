620ea46b76ef54fddf05792dafe27ece5fa3c82b
ironpup <christoph.ding@gmail.com>
2018-02-10 18:23:21 -0500

    Make a spiffy grid generator.

--- algos-by-week/2.4.17/uniquePaths.js
+++ algos-by-week/2.4.17/uniquePaths.js
@@ -1,8 +1,31 @@
 function robotPaths(height, width) {
   let numberOfPaths = 0
 
+  let grid = makeGrid(height, width)
+
+
+
+
+
+
   return numberOfPaths
 }
 
+function makeGrid(height, width) {
+  let grid = new Array(height)
+
+  let size = grid.length
+  while (size--) {
+    grid[size] = new Array(width)
+  }
+
+  return grid
+}
+
+
 // tests
-console.log(robotPaths(2, 2)) // 2
+// console.log(robotPaths(2, 2)) // 2
+// console.log(makeGrid(2, 2))
+let grid = makeGrid(2, 2)
+console.log(grid)
+
