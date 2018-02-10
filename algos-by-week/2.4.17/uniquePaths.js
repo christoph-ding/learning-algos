d0f34aecad1f6008c8f906f3f048fea97c1d505d
ironpup <christoph.ding@gmail.com>
2018-02-10 18:55:59 -0500

    Implemented uniquePaths with dynamic programming.

--- algos-by-week/2.4.17/uniquePaths.js
+++ algos-by-week/2.4.17/uniquePaths.js
@@ -1,14 +1,26 @@
 function robotPaths(height, width) {
-  let numberOfPaths = 0
-
   let grid = makeGrid(height, width)
 
+  // TraverseGrid
+  grid.forEach(function(row, yPos) {
+    console.log('yPos: ', yPos)
 
-
-
-
-
-  return numberOfPaths
+    for (let xPos = 0; xPos < width; xPos++) {
+      // bounds
+      let totalNumberOfPathsToThisCell;
+      if (xPos === 0 || yPos === 0) {        
+        totalNumberOfPathsToThisCell = 1
+      } else {
+        // each cell should be sum of up 1, left 1
+        let upOneNumPaths = grid[yPos - 1][xPos]
+        let leftOneNumberPaths = grid[yPos][xPos - 1] 
+        totalNumberOfPathsToThisCell = upOneNumPaths + leftOneNumberPaths
+      }
+      row[xPos] = totalNumberOfPathsToThisCell
+    }
+  })
+ 
+  return grid[height - 1][width - 1]
 }
 
 function makeGrid(height, width) {
@@ -22,10 +34,6 @@ function makeGrid(height, width) {
   return grid
 }
 
-
 // tests
-// console.log(robotPaths(2, 2)) // 2
-// console.log(makeGrid(2, 2))
-let grid = makeGrid(2, 2)
-console.log(grid)
+console.log(robotPaths(3, 3)) // 2
 
